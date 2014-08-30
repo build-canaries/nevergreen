4f6b6937c7f67aa02898478e34506ed80c9e33a4
Joe Wright <joe@joejag.com>
2014-08-30 21:23:42 +0100

    Introduce JS enclosing functions to separate Styling from DOM manipulation

--- resources/public/js/monitor.js
+++ resources/public/js/monitor.js
@@ -1,53 +1,59 @@
-var maxColumns = 3
-var buildStatusPadding = 10
-var fontResizeFactor = 1.6
+function Styler() {
+    this.maxColumns = 3
+    this.buildStatusPadding = 10
+    this.fontResizeFactor = 1.6
 
-function buildStatusCount() {
-    return $('li').size()
-}
+    function buildStatusCount() {
+        return $('li').size()
+    }
 
-function numberOfColumns() {
-    return Math.min(maxColumns, buildStatusCount())
-}
+    function numberOfColumns() {
+        return Math.min(maxColumns, buildStatusCount())
+    }
 
-function numberOfRows() {
-    return Math.ceil(buildStatusCount() / maxColumns)
-}
+    function numberOfRows() {
+        return Math.ceil(buildStatusCount() / maxColumns)
+    }
 
-function buildStatusWidth() {
-    return window.innerWidth / numberOfColumns() - buildStatusPadding
-}
+    function buildStatusWidth() {
+        return window.innerWidth / numberOfColumns() - buildStatusPadding
+    }
 
-function buildStatusHeight() {
-    return window.innerHeight / numberOfRows() - buildStatusPadding
-}
+    function buildStatusHeight() {
+        return window.innerHeight / numberOfRows() - buildStatusPadding
+    }
 
-function scaleFontToContainerSize() {
-    $(".outerContainer").fitText(fontResizeFactor)
-}
+    function scaleFontToContainerSize() {
+        $(".outerContainer").fitText(fontResizeFactor)
+    }
 
-function styleListItems() {
-    $('.outerContainer').height(buildStatusHeight()).width(buildStatusWidth())
-    scaleFontToContainerSize()
-}
-
-function addBuildStatusToScreen(project) {
-    var buildStatus = project.lastBuildStatus
-    if(buildStatus !== "Success"){
-        $('#projects').append("<li><div class=outerContainer><div class=innerContainer>" +
-        project.name + "</div></div></li>")
+    this.styleListItems = function () {
+        $('.outerContainer').height(buildStatusHeight()).width(buildStatusWidth())
+        scaleFontToContainerSize()
     }
 }
 
-function addListItems(data) {
-    $('#projects').empty()
-    data.body.forEach(addBuildStatusToScreen)
+function StatusAppender(projects) {
+    this.projects = projects
+
+    function addBuildStatusToScreen(project) {
+        var buildStatus = project.lastBuildStatus
+        if(buildStatus !== "Success"){
+            $('#projects').append("<li><div class=outerContainer><div class=innerContainer>" +
+            project.name + "</div></div></li>")
+        }
+    }
+
+    this.addListItems = function() {
+        $('#projects').empty()
+        projects.forEach(addBuildStatusToScreen)
+    }
 }
 
 function updateBuildMonitor() {
     $.getJSON("/projects").then(function(data){
-        addListItems(data)
-        styleListItems()
+        new StatusAppender(data.body).addListItems()
+        new Styler().styleListItems()
     })
 }
 
