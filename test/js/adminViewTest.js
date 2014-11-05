c0cbe89592d96096b1cb29b096dc34addd0133e7
Ste Cowley <ste@thoughtworks.com>
2014-11-05 13:19:16 +0000

    Ste/Joe - In progress view test

---
+++ test/js/adminViewTest.js
@@ -0,0 +1,11 @@
+describe("view logic", function () {
+
+    it("adds click listener to page", function () {
+        $("body").append("<input id='save-projects'/>")
+
+        new AdminView().init()
+
+//        expect($("#"))
+    })
+
+})
