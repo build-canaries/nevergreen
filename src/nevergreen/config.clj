a7ce968ec75e9de8872420a897e6b524a44831ea
Joe Wright <joe@joejag.com>
2014-11-26 22:15:11 +0000

    Joe - delete unused function

--- src/nevergreen/config.clj
+++ src/nevergreen/config.clj
@@ -2,10 +2,5 @@
   (:require [clojure.string :refer [split trim]]
             [environ.core :refer [env]]))
 
-(defn- split-string [s]
-  (if s
-    (set (map trim (split s #",")))
-    #{}))
-
 (defn port []
   (Integer. (or (env :port) 5000)))
