a818d52148b9c611d115b1cbc643a47d2765acf8
Joe Wright <joe@joejag.com>
2014-12-03 00:55:11 +0000

    Joe closes #27 multiple displays of the same project bug. New call to clj-cctray lib required

--- src/nevergreen/api.clj
+++ src/nevergreen/api.clj
@@ -1,15 +1,19 @@
 (ns nevergreen.api
   (:require [clj-cctray.core :as parser]
             [clj-cctray.filtering :as filtering]
+            [clj-cctray.ci.go-snap :as go]
             [cheshire.core :refer [generate-string]]
             [environ.core :refer [env]]
             [nevergreen.config :refer :all]
             [compojure.core :refer :all]))
 
 (defn get-all-projects [url]
-  (parser/get-projects url :options [:go :normalise]))
+  (->>
+    (parser/get-projects url :options [:go :normalise])
+    (go/distinct-projects)))
 
 (defn get-interesting-projects [params]
   (->> (parser/get-projects (:cctray params) :options [:go :normalise])
+       (go/distinct-projects)
        (filtering/interesting)
        (filtering/by-name (:includedProjects params))))
