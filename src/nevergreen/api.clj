8c1862115e3e4cf4b62278caa0b5b1057272595b
Joe Wright <joe@joejag.com>
2014-12-08 22:52:15 +0000

    Joe #30 validate URL being passed in is http url

--- src/nevergreen/api.clj
+++ src/nevergreen/api.clj
@@ -6,7 +6,13 @@
 (defn detect-server [projects]
   (if (some (fn [project] (re-find #" :: " (:name project))) projects) "go" ""))
 
+(defn invalid-url? [url]
+  (or (blank? url)
+      (not (re-find #"https?://" url))))
+
 (defn get-all-projects [url]
+  (if (invalid-url? url) (throw (IllegalArgumentException. "Not a valid url")))
+
   (let [projects (parser/get-projects url {:normalise :all})
         server (detect-server projects)]
     (if (= server "go")
@@ -19,6 +25,9 @@
     {:normalise :all}))
 
 (defn get-interesting-projects [params]
+  (if (invalid-url? (:cctray params)) (throw (IllegalArgumentException. "Not a valid url")))
+
   (->> (parser/get-projects (:cctray params) (options-from-config params))
        (filtering/interesting)
        (filtering/by-name (:includedProjects params))))
+
--- test/nevergreen/api_test.clj
+++ test/nevergreen/api_test.clj
@@ -3,25 +3,32 @@
             [nevergreen.api :as subject]
             [clj-cctray.core :as parser]))
 
+(def valid-cctray "http://someserver/cc.xml")
+
 (fact "it finds interesting projects"
-      (subject/get-interesting-projects {:includedProjects ["project-1"]}) => (list {:name "project-1" :prognosis :sick})
+      (subject/get-interesting-projects {:includedProjects ["project-1"] :cctray valid-cctray}) => (list {:name "project-1" :prognosis :sick})
       (provided
         (parser/get-projects anything anything) => [{:name "project-1" :prognosis :sick}]))
 
 (facts "we can detect if we are using a go server"
        (fact "it is not a go server"
-             (subject/get-all-projects anything) => (contains {:server ""})
+             (subject/get-all-projects valid-cctray) => (contains {:server ""})
              (provided
                (parser/get-projects anything {:normalise :all}) => [{:name "project-1"}]))
 
        (fact "it is a go server"
-             (subject/get-all-projects anything) => (contains {:server "go"})
+             (subject/get-all-projects valid-cctray) => (contains {:server "go"})
              (provided
                (parser/get-projects anything anything) => [{:name "project-1"} {:name "project-1 :: job1"}])))
 
 (facts "parses requested serverType"
-
        (fact "converts go server param to symbol"
              (subject/options-from-config {:serverType "go"}) => {:normalise :all, :server :go})
        (fact "gives nil for empty string"
              (subject/options-from-config {:serverType ""}) => {:normalise :all}))
+
+(facts "check url is invalid"
+       (fact (subject/invalid-url? "http://bleh") => false)
+       (fact (subject/invalid-url? "https://bleh") => false)
+       (fact (subject/invalid-url? "gopher://bleh") => true)
+       (fact (subject/invalid-url? nil) => true))
