9ee49d7b4b848384e999cd856ddb3ab028f3ccd1
Christopher Martin <cmartin@thoughtworks.com>
2015-01-30 16:33:34 +0000

    Chris #43 - Adds server namespace for detecting server type

---
+++ src/nevergreen/servers.clj
@@ -0,0 +1,17 @@
+(ns nevergreen.servers)
+
+(defn detect-server [url]
+  (cond
+    (nil? url) nil
+    (.contains url "//jenkins") :jenkins
+    (.contains url "//hudson") :hudson
+    (.contains url "//travis-ci.org") :travis
+    (.contains url "/go/") :go
+    (.contains url "//snap-ci.com") :snap
+    (.contains url "//circleci.com") :circle
+    (.contains url "//teamcity") :team-city
+    (.contains url "//cc.rb.") :cruise-control-rb
+    (.contains url "//cc.java.") :cruise-control
+    (.contains url "//cc.net.") :cruise-control-net
+    (.contains url "//api.tddium.com") :solano
+    :else :unknown))
