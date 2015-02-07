c298ad7f6a5bc719d2cde8f99509f052c79a50dd
Joe Wright <joe@joejag.com>
2015-02-07 17:03:54 +0000

    Joe #38 use base64 lib rather than java.util.Base64 which only exists in Java8

--- src/nevergreen/crypto.clj
+++ src/nevergreen/crypto.clj
@@ -1,25 +1,31 @@
 (ns nevergreen.crypto
-  (:require [nevergreen.config :as config])
+  (:require [nevergreen.config :as config]
+            [base64-clj.core :as base64])
   (import (java.security Key)
           (javax.crypto Cipher)
-          (javax.crypto.spec SecretKeySpec)
-          (java.util Base64)))
+          (javax.crypto.spec SecretKeySpec)))
 
-(defn- bytes-to-base64 [bytes]
-  (String. (.encode (Base64/getEncoder) (byte-array bytes))))
+(defn- bytes->base64 [bytes]
+  (String. (base64/encode-bytes bytes)))
 
-(defn- base64-to-bytes [^String b64]
-  (bytes (byte-array (map (comp byte int) (.decode (Base64/getDecoder) b64)))))
+(defn- base64->bytes [^String b64]
+  (base64/decode-bytes (.getBytes b64)))
+
+(def transformation "AES/ECB/PKCS5Padding")
+(def charset "US-ASCII")
 
 (defn- ^Key secret [^String aes-key]
   (SecretKeySpec. (.getBytes aes-key) "AES"))
 
+(defn- cipher [mode]
+  (doto (Cipher/getInstance transformation)
+    (.init mode (secret (config/aes-key)))))
+
 (defn ^bytes encrypt [^String plain-text]
-  (let [cipher (doto (Cipher/getInstance "AES/ECB/PKCS5Padding")
-                 (.init Cipher/ENCRYPT_MODE (secret (config/aes-key))))]
-    (bytes-to-base64 (.doFinal cipher (.getBytes plain-text "US-ASCII")))))
+  (let [cipher (cipher Cipher/ENCRYPT_MODE)]
+    (bytes->base64 (.doFinal cipher (.getBytes plain-text charset)))))
 
 (defn ^String decrypt [^String base64-encoded]
-  (let [cipher (doto (Cipher/getInstance "AES/ECB/PKCS5Padding")
-                 (.init Cipher/DECRYPT_MODE (secret (config/aes-key))))]
-    (String. (.doFinal cipher (base64-to-bytes base64-encoded)) "US-ASCII")))
+  (let [cipher (cipher Cipher/DECRYPT_MODE)]
+    (String. (.doFinal cipher (base64->bytes base64-encoded)) charset)))
+
