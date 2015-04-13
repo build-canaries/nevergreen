61164757f3ddd22b219262c21de5c6b339da57f4
Christopher Martin <cmartin@thoughtworks.com>
2015-04-13 22:31:49 +0100

    Chris - adds tests for the trays controller

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'browserify'],
+    frameworks: ['jasmine', 'browserify', 'phantomjs-shim'],
 
     // list of files / patterns to load in the browser
     files: [
