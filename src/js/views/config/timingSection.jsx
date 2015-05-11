f61431bdc39b200fa4e91500abb92d79a677b36a
Christopher Martin <cmartin@thoughtworks.com>
2015-05-11 21:03:01 +0100

    Chris - minor styling update

--- src/js/views/config/timingSection.jsx
+++ src/js/views/config/timingSection.jsx
@@ -1,9 +1,10 @@
-var React = require('react')
-var $ = require('jquery')
+var React = require('react/addons')
 var timingRepository = require('../../storage/timingRepository')
 
 module.exports = {
     TimingSection: React.createClass({
+        mixins: [React.addons.LinkedStateMixin],
+
         getInitialState: function () {
             return {pollingTime: timingRepository.getPollingTime()}
         },
@@ -11,25 +12,16 @@ module.exports = {
         render: function () {
             return (
                 <section id="timing" className="dashboard-main-section">
-                    <fieldset className="tracking-cctray-group">
-                        <div className="tracking-cctray-group-cctray-form">
-                            <div>
-                                <label htmlFor="polling-time">Poll for project changes every</label>
-                                <input id="polling-time" className="tracking-cctray-group-cctray-form-input-small tracking-cctray-group-cctray-form-input" type="number" min="1" value={this.state.pollingTime} onChange={this.onChange}/>
-                                seconds
-                            </div>
+                    <h3 className='success-title'>Polling</h3>
+                    <div className="tracking-cctray-group-cctray-form">
+                        <div>
+                            <label htmlFor="polling-time">Poll for project changes every</label>
+                            <input id="polling-time" className="tracking-cctray-group-cctray-form-input-small tracking-cctray-group-cctray-form-input" type="number" min="1" max="99" valueLink={this.linkState('pollingTime')} />
+                            seconds
                         </div>
-                    </fieldset>
+                    </div>
                 </section>
             )
-        },
-
-        componentWillMount: function () {
-            this.setState({pollingTime: timingRepository.getPollingTime()})
-        },
-
-        onChange: function (event) {
-            this.setState({pollingTime: event.target.value})
         },
 
         componentWillUpdate: function (nextProps, nextState) {
