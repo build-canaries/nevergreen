bd4e2671c3a240864d458bce10283c0afc39b3bb
Christopher Martin <cmartin@thoughtworks.com>
2015-07-04 11:10:30 +0100

    Chris - minor changes to make input more obvious

--- update-version.sh
+++ update-version.sh
@@ -5,13 +5,20 @@ echo "This script can be used to update the version number in all the required f
 echo "It's very hacky and a better method should be found!"
 echo
 
-read -p "major? " major
-read -p "minor? " minor
-read -p "revision? " revision
-read -p "name? " name
-read -p "hex value? " hex
+read -p "major? [a number] " major
+read -p "minor? [a number] " minor
+read -p "revision? [a number] " revision
+read -p "pre-release? [y/n] " pre
+echo "Leave the following values blank if they haven't changed since the last release"
+read -p "name? [string] " name
+read -p "colour? [hex value e.g. #000000] " hex
 
-version="$major.$minor.$revision"
+case $pre in
+    [Yy]* ) pre='-alpha';;
+    [Nn]* ) pre='';;
+esac
+
+version="$major.$minor.$revision$pre"
 
 echo "Updating to version $version $name ..."
 
@@ -26,12 +33,28 @@ sed -i '' "s|defproject nevergreen \"[^\"]*\"|defproject nevergreen \"$version\"
 echo "Updating file ./package.json ..."
 sed -i '' "s|\"version\": \"[^\"]*\"|\"version\": \"$version\"|" ./package.json
 
-echo "Updating file ./src/nevergreen/app/routes.clj ..."
-sed -i '' "s|releases/download/[^/]*/|releases/download/v$version/|" ./src/nevergreen/app/routes.clj
+if [ -z "$pre" ]
+then
+    echo "Updating file ./src/nevergreen/app/routes.clj ...";
+    sed -i '' "s|releases/download/[^/]*/|releases/download/v$version/|" ./src/nevergreen/app/routes.clj;
+else
+    echo "Skipping file ./src/nevergreen/app/routes.clj as pre-release is true"
+fi
 
 echo "Updating file ./src/js/views/general/menu.jsx ..."
 sed -i '' "s|<p id='version-number'>[^<]*<|<p id='version-number'>v$version<|" ./src/js/views/general/menu.jsx
-sed -i '' "s|<p id='version-name'>[^<]*<|<p id='version-name'>$name<|" ./src/js/views/general/menu.jsx
 
-echo "Updating file ./src/scss/base/_variables.scss ..."
-sed -i '' "s|\$version.*|\$version: $hex\;|" ./src/scss/base/_variables.scss
+if [ -z "$name" ]
+then
+    echo "Skipping updating name in file ./src/js/views/general/menu.jsx as name was blank"
+else
+    sed -i '' "s|<p id='version-name'>[^<]*<|<p id='version-name'>$name<|" ./src/js/views/general/menu.jsx
+fi
+
+if [ -z "$hex" ]
+then
+    echo "Skipping file ./src/scss/base/_variables.scss as colour was blank"
+else
+    echo "Updating file ./src/scss/base/_variables.scss ..."
+    sed -i '' "s|\$version.*|\$version: $hex\;|" ./src/scss/base/_variables.scss
+fi
