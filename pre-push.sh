2a880eaa8b3c1ba6c9700fdd506897caf2b5de49
Christopher Martin <cmartin@thoughtworks.com>
2015-10-05 16:54:28 +0100

    Chris - actually compile sass and js

--- pre-push.sh
+++ pre-push.sh
@@ -10,23 +10,26 @@ hash lein 2>/dev/null || {
     exit 1
 }
 
-echo '[Step 1 of 6] Updating node packages...'
+echo '[Step 1 of 7] Updating node packages...'
 npm install
 
-echo '[Step 2 of 6] Running the ui unit tests...'
+echo '[Step 2 of 7] Compiling...'
+npm run sass && npm run jsx && npm run browserify
+
+echo '[Step 3 of 7] Running the ui unit tests...'
 npm test
 
-echo '[Step 3 of 6] Linting the JavaScript...'
+echo '[Step 4 of 7] Linting the JavaScript...'
 npm run lint
 
-echo '[Step 4 of 6] Running the server unit tests...'
+echo '[Step 5 of 7] Running the server unit tests...'
 lein do clean, midje 'nevergreen.*'
 
-echo '[Step 5 of 6] Running the functional tests (the server must be running!)...'
+echo '[Step 6 of 7] Running the functional tests (the server must be running!)...'
 export JVM_OPTS="-Dwebdriver.chrome.driver=./node_modules/chromedriver/bin/chromedriver"
 lein test functional.functional-test
 
-echo '[Step 6 of 6] Building an uberjar...'
+echo '[Step 7 of 7] Building an uberjar...'
 lein uberjar
 
 echo 'Done! Check the output for any errors!'
