66b6742c176541a4b165f8b3a1a5efde65d475e3
Christopher Martin <cmartin@thoughtworks.com>
2015-10-14 18:22:32 +0100

    Chris - adds -e flag so it hopefully runs on ci

--- update-version.sh
+++ update-version.sh
@@ -27,26 +27,26 @@ version="$major.$minor.$revision"
 echo "Updating to version $version $name ..."
 
 echo "Updating file ./project.clj ..."
-sed -i '' "s|defproject nevergreen \"[^\"]*\"|defproject nevergreen \"$version\"|" ./project.clj
+sed -i '' -e "s|defproject nevergreen \"[^\"]*\"|defproject nevergreen \"$version\"|" ./project.clj
 
 echo "Updating file ./package.json ..."
-sed -i '' "s|\"version\": \"[^\"]*\"|\"version\": \"$version\"|" ./package.json
+sed -i '' -e "s|\"version\": \"[^\"]*\"|\"version\": \"$version\"|" ./package.json
 
 echo "Updating file ./src/js/views/app.jsx ..."
-sed -i '' "s|versionNumber: '[^']*'|versionNumber: '$version'|" ./src/js/views/app.jsx
+sed -i '' -e "s|versionNumber: '[^']*'|versionNumber: '$version'|" ./src/js/views/app.jsx
 
 if [ -z "$name" ]
 then
     echo "Skipping updating name in file ./src/js/views/app.jsx as name was blank"
 else
-    sed -i '' "s|versionName: '[^']*'|versionName: '$name'|" ./src/js/views/app.jsx
+    sed -i '' -e "s|versionName: '[^']*'|versionName: '$name'|" ./src/js/views/app.jsx
 fi
 
 if [ -z "$hash" ]
 then
     echo "Skipping updating commit hash in file ./src/js/views/app.jsx as hash was blank"
 else
-    sed -i '' "s|commitHash: '[^']*'|commitHash: '$hash'|" ./src/js/views/app.jsx
+    sed -i '' -e "s|commitHash: '[^']*'|commitHash: '$hash'|" ./src/js/views/app.jsx
 fi
 
 if [ -z "$hex" ]
@@ -54,5 +54,5 @@ then
     echo "Skipping file ./src/scss/base/_variables.scss as colour was blank"
 else
     echo "Updating file ./src/scss/base/_variables.scss ..."
-    sed -i '' "s|\$version.*|\$version: $hex\;|" ./src/scss/base/_variables.scss
+    sed -i '' -e "s|\$version.*|\$version: $hex\;|" ./src/scss/base/_variables.scss
 fi
