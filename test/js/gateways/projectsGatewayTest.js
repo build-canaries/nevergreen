e16cf2e6bd43b0a82bef7097f56efad3c58002f7
Jimmy Thompson <jimmy@jimmythompson.co.uk>
2015-12-09 21:23:39 +0000

    Jimmy - Added test for existing project gateway API

--- test/js/gateways/projectsGatewayTest.js
+++ test/js/gateways/projectsGatewayTest.js
@@ -1,12 +1,27 @@
 jest.dontMock('../../../src/js/gateways/projectsGateway')
 
-describe('repository', function () {
+describe('projects gateway', function () {
 
   var subject, gateway
 
   beforeEach(function () {
     subject = require('../../../src/js/gateways/projectsGateway')
     gateway = require('../../../src/js/gateways/gateway')
+  })
+
+  describe('getting all projects', function () {
+    it('has all data', function () {
+      var tray = {
+        url: 'url',
+        username: 'uname',
+        password: 'pword',
+        serverType: 'GO'
+      }
+
+      subject.fetchAll(tray)
+
+      expect(gateway.post).toBeCalledWith('/api/projects/all', tray)
+    })
   })
 
   describe('getting interesting projects', function () {
