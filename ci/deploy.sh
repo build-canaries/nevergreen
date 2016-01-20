b67eea43acee072a298012e6ba866714ef0469a5
Christopher Martin <cmartin@thoughtworks.com>
2016-01-20 10:53:25 +0000

    Chris - adds scripts for deploy and release stages

---
+++ ci/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+# deploy to staging
+scp -i /var/go/nevergreen_rsa ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/staging
+ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo /etc/init.d/nevergreen-staging restart"
---
+++ ci/release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+# deploy to production
+scp -i /var/go/nevergreen_rsa ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/production
+ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo /etc/init.d/nevergreen-production restart"
