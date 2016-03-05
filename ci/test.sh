8af2d1626133880f21755706fb17b134838b7ed5
Christopher Martin <cmartin@thoughtworks.com>
2016-03-05 13:38:55 +0000

    Chris - use ci cache to speed up build

--- ci/test.sh
+++ ci/test.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -e
 
+# restore cached node_modules if it exists
+[[ -d ${SNAP_CACHE_DIR}/node_modules ]] && mv -f "${SNAP_CACHE_DIR}/node_modules" node_modules
+
 # download ui dependencies
 npm prune
 npm install
+
+# copy node_modules back to cache
+[[ -d ${SNAP_CACHE_DIR} ]] && mv -f ${SNAP_CACHE_DIR} node_modules
+
+# restore cached maven repo if it exists
+[[ -d ${SNAP_CACHE_DIR}/.m2 ]] && mv -f "${SNAP_CACHE_DIR}/.m2" ~/.m2
+
+# download server dependencies
+lein deps
+
+# copy the maven repo back to cache
+[[ -d ${SNAP_CACHE_DIR} ]] && mv -f ${SNAP_CACHE_DIR} ~/.m2
 
 # run ui tests
 npm run lint
