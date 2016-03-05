c3295c42cc482c26bc2ab900bb97aca8119e36ca
Christopher Martin <cmartin@thoughtworks.com>
2016-03-05 14:07:49 +0000

    Chris - use copy command instead of move...

--- ci/test.sh
+++ ci/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 # restore cached node_modules if it exists
-[[ -d ${SNAP_CACHE_DIR}/node_modules ]] && mv -f ${SNAP_CACHE_DIR}/node_modules ${SNAP_WORKING_DIR}/node_modules
+[[ -d ${SNAP_CACHE_DIR}/node_modules ]] && cp -R ${SNAP_CACHE_DIR}/node_modules/ ${SNAP_WORKING_DIR}/node_modules/
 
 # download ui dependencies
 npm prune
 npm install
 
 # copy node_modules back to cache
-[[ -d ${SNAP_CACHE_DIR} ]] && mv -f ${SNAP_WORKING_DIR}/node_modules ${SNAP_CACHE_DIR}/node_modules
+[[ -d ${SNAP_CACHE_DIR} ]] && cp -R ${SNAP_WORKING_DIR}/node_modules/ ${SNAP_CACHE_DIR}/node_modules/
 
 # run ui tests
 npm run lint
