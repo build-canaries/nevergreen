28a51ebe8f85a12ab5452130a6cffd356abfc386
Darren Haken <darrenhaken@gmail.com>
2016-04-21 14:48:22 -0700

    Darren - Add play broken build settings to UI

--- test/js/actions/DisplayActionsTest.js
+++ test/js/actions/DisplayActionsTest.js
@@ -10,12 +10,21 @@ describe('display actions', () => {
     Constants = require('../../../src/js/constants/NevergreenConstants')
   })
 
-  it('dispatches an action with the value given', () => {
+  it('dispatches an action for broken build timers', () => {
     subject.setBrokenBuildTimers('some-value')
 
     expect(AppDispatcher.dispatch).toBeCalledWith({
       type: Constants.BrokenBuildTimersChanged,
       value: 'some-value'
+    })
+  })
+
+  it('dispatches an action for the broken build sounds toggled', () => {
+    subject.setBrokenBuildSounds('toggle on')
+
+    expect(AppDispatcher.dispatch).toBeCalledWith({
+      type: Constants.BrokenBuildSoundsToggled,
+      value: 'toggle on'
     })
   })
 })
