72b321825470b608c6b6665aff9374ccbc1b94e1
Joe Wright <joe@joejag.com>
2016-09-18 17:12:58 +0100

    Update release scripts to use new way of restarting the services on production

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,7 +2,7 @@
 
 # deploy to staging
 scp -i /var/go/nevergreen_rsa ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/staging
-ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo /etc/init.d/nevergreen-staging restart"
+ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo service nevergreen-staging restart"
 
 # smoke test
 ./ci/smoke-test.sh https://staging.nevergreen.io/api/ping
--- ci/release.sh
+++ ci/release.sh
@@ -2,7 +2,7 @@
 
 # deploy to production
 scp -i /var/go/nevergreen_rsa ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/production
-ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo /etc/init.d/nevergreen-production restart"
+ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo service nevergreen restart"
 
 # smoke test
 ./ci/smoke-test.sh https://nevergreen.io/api/ping
