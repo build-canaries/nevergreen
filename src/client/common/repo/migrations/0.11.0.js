2c80dcd5dc758a33f0bf8312d104b44c7fff704d
Christopher Martin <cmartin@thoughtworks.com>
2016-10-14 22:30:32 +0100

    removes comments

--- src/client/common/repo/migrations/0.11.0.js
+++ src/client/common/repo/migrations/0.11.0.js
@@ -89,34 +89,3 @@ export function migrate(data) {
 
   return data
 }
-
-/*
- NEW
-
- "projects": {
- "2408514d-ee41-42e1-bc09-da7faad77caf": {
- "https://builds.apache.org/job/Knox-master-patch-scan/": {
- "removed": false,
- "name": "knox master patch scan",
- "projectId": "https://builds.apache.org/job/Knox-master-patch-scan/",
- "isNew": true,
- "stage": null
- }
- }
- }
- */
-
-/*
- OLD
-
- "fetchedProjects": {
- "69d235ed-c2db-4f55-9ba5-5dbf1a651be1": [
- {
- "projectId": "a25veCBtYXN0ZXIgcGF0Y2ggc2Nhbi8v",
- "name": "knox master patch scan",
- "isNew": false,
- "wasRemoved": false
- }
- ]
- }
- */
