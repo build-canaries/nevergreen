ac7a1d1c868f5ac4c077a35c1ae3cec33194e402
Christopher Martin <cmartin@thoughtworks.com>
2016-10-28 23:49:06 +0100

    adds a unit test for available project

---
+++ test/client/tracking/tray/AvailableProjectTest.js
@@ -0,0 +1,38 @@
+import '../../UnitSpec'
+import {describe, it} from 'mocha'
+import {expect} from 'chai'
+import React from 'react'
+import {shallow} from 'enzyme'
+import AvailableProject from '../../../../src/client/tracking/tray/AvailableProject'
+
+describe('<AvailableProject/>', function () {
+  const DEFAULT_PROPS = {
+    name: '',
+    stage: null,
+    isNew: null,
+    removed: null,
+    selected: null,
+    selectProject: () => {
+    }
+  }
+
+  it('should render new tag if the project is new', function () {
+    const props = Object.assign({}, DEFAULT_PROPS, {isNew: true})
+    const wrapper = shallow(<AvailableProject {...props} />)
+    expect(wrapper.find('.new-project')).to.be.present()
+    expect(wrapper.find('.removed-project')).to.not.be.present()
+  })
+
+  it('should render removed tag if the project was removed', function () {
+    const props = Object.assign({}, DEFAULT_PROPS, {removed: true})
+    const wrapper = shallow(<AvailableProject {...props} />)
+    expect(wrapper.find('.new-project')).to.not.be.present()
+    expect(wrapper.find('.removed-project')).to.be.present()
+  })
+
+  it('should render the stage', function () {
+    const props = Object.assign({}, DEFAULT_PROPS, {name: 'name', stage: 'stage'})
+    const wrapper = shallow(<AvailableProject {...props} />)
+    expect(wrapper).to.contain.text('name stage')
+  })
+})
