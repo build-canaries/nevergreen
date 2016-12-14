6db6969020d806b9db50ab1a47d0f71f046d58c3
Christopher Martin <cmartin@thoughtworks.com>
2016-12-14 21:42:45 +0000

    fixes warnings in tests

--- test/client/monitor/InterestingProjectsTest.js
+++ test/client/monitor/InterestingProjectsTest.js
@@ -17,7 +17,7 @@ describe('<InterestingProjects/>', function () {
 
   it('should render broken build sound fx if its enabled and any project is broken', function () {
     const props = Object.assign({}, DEFAULT_PROPS, {
-      projects: [{projectId: 'someId', prognosis: 'sick'}],
+      projects: [{projectId: 'someId', prognosis: 'sick', name: '', lastBuildTime: ''}],
       trays: [{someId: {name: 'some-tray-name'}}],
       playBrokenBuildSounds: true,
       brokenBuildFx: 'some-sfx'
@@ -28,7 +28,7 @@ describe('<InterestingProjects/>', function () {
 
   it('should not render broken build sound fx if its disabled even if any project is sick', function () {
     const props = Object.assign({}, DEFAULT_PROPS, {
-      projects: [{projectId: 'someId', prognosis: 'sick'}],
+      projects: [{projectId: 'someId', prognosis: 'sick', name: '', lastBuildTime: ''}],
       trays: [{someId: {name: 'some-tray-name'}}],
       playBrokenBuildSounds: false,
     })
@@ -38,7 +38,7 @@ describe('<InterestingProjects/>', function () {
 
   it('should not render broken build sound fx if its enabled but no projects are sick', function () {
     const props = Object.assign({}, DEFAULT_PROPS, {
-      projects: [{projectId: 'someId', prognosis: 'healthy'}],
+      projects: [{projectId: 'someId', prognosis: 'healthy', name: '', lastBuildTime: ''}],
       trays: [{someId: {name: 'some-tray-name'}}],
       playBrokenBuildSounds: false,
     })
