a24e8735478ca3f9f9b8fc7eb8e849563f42bd39
Christopher Martin <eternalepoch@gmail.com>
2017-02-09 21:12:13 +0000

    replaces fetch with superagent as it supports timeouts and aborting requests

--- src/client/common/gateways/Gateway.js
+++ src/client/common/gateways/Gateway.js
@@ -1,49 +1,34 @@
+import request from 'superagent'
 import Promise from 'promise'
-import {toJson} from '../Json'
 
+const THIRTY_SECONDS = 1000 * 30
 const THREE_MINUTES = 1000 * 60 * 60 * 3
+const TIMEOUT = {
+  response: THIRTY_SECONDS,
+  deadline: THREE_MINUTES
+}
 const ACCEPT_HEADER = 'application/json; charset=utf-8'
 
-function withTimeout(promise) {
-  return Promise.race([promise, new Promise((_, reject) => setTimeout(reject, THREE_MINUTES))])
-}
-
-function timeoutError() {
-  return Promise.reject({
-    status: 0,
-    message: 'Connection timeout'
-  })
-}
-
-function processStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response)
-  } else {
-    return response.text().then((body) => {
-      return Promise.reject({
-        status: response.status,
-        message: body
-      })
-    })
-  }
+function mapError(err) {
+  return Promise.reject({status: err.status, message: err.response.text})
 }
 
 export function post(url, data) {
-  return withTimeout(fetch(url, {
-    method: 'post',
-    body: toJson(data),
-    headers: {
-      Accept: ACCEPT_HEADER,
-      'Content-Type': 'application/json; charset=utf-8'
-    }
-  })).catch(timeoutError)
-    .then(processStatus)
-    .then((response) => response.json())
+  return request
+    .post(url)
+    .send(data)
+    .accept(ACCEPT_HEADER)
+    .type('application/json; charset=utf-8')
+    .timeout(TIMEOUT)
+    .then((res) => res.body)
+    .catch(mapError)
 }
 
 export function get(url) {
-  return withTimeout(fetch(url, {headers: {Accept: ACCEPT_HEADER}}))
-    .catch(timeoutError)
-    .then(processStatus)
-    .then((response) => response.json())
+  return request
+    .get(url)
+    .accept(ACCEPT_HEADER)
+    .timeout(TIMEOUT)
+    .then((res) => res.body)
+    .catch(mapError)
 }
