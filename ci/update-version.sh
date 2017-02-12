faf4d3c027983ee511a8a7f474cd583e84c0d642
Christopher Martin <eternalepoch@gmail.com>
2017-02-12 16:26:40 +0000

    [#161] combines the update verion scripts using generic env variables
    [#161] combines the update verion scripts using generic env variables

--- ci/update-version.sh
+++ ci/update-version.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
 
-meta="$SNAP_PIPELINE_COUNTER"
-hash="$SNAP_COMMIT_SHORT"
-
-echo "Updating commit hash and meta in file ./package.json ..."
-sed -i.bak "s|\"versionMeta\": \"[^\"]*\"|\"versionMeta\": \"$meta\"|" package.json
-sed -i.bak "s|\"commitHash\": \"[^\"]*\"|\"commitHash\": \"$hash\"|" package.json
+echo "Updating commit hash [$COMMIT_HASH] and meta [$BUILD_NUM] in file ./package.json ..."
+sed -i.bak "s|\"versionMeta\": \"[^\"]*\"|\"versionMeta\": \"$BUILD_NUM\"|" package.json
+sed -i.bak "s|\"commitHash\": \"[^\"]*\"|\"commitHash\": \"$COMMIT_HASH\"|" package.json
 
 find . -type f -name "*.bak" -exec rm -f {} \;
