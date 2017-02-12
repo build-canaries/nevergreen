04ddea5a18676cf7e27e4a10c6bb1cc346bfb86c
Christopher Martin <eternalepoch@gmail.com>
2017-02-12 23:12:15 +0000

    [#161] stop setting the ssh key to use explicitly

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # deploy to staging
-scp -i /var/go/nevergreen_rsa ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/staging
-ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo service nevergreen-staging restart"
+scp ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/staging
+ssh nevergreen@nevergreen.io "sudo service nevergreen-staging restart"
 
 # smoke test
 ./ci/smoke-test.sh https://staging.nevergreen.io/api/ping
--- ci/release.sh
+++ ci/release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 # deploy to production
-scp -i /var/go/nevergreen_rsa ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/production
-ssh -i /var/go/nevergreen_rsa nevergreen@nevergreen.io "sudo service nevergreen restart"
+scp ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/production
+ssh nevergreen@nevergreen.io "sudo service nevergreen restart"
 
 # smoke test
 ./ci/smoke-test.sh https://nevergreen.io/api/ping
