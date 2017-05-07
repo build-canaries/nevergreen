f251534432117f3faf5175ba18e07dd9d2bc4ee8
Christopher Martin <eternalepoch@gmail.com>
2017-05-07 02:46:40 +0100

    updates server deps

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "A build monitor with attitude"
   :url "https://github.com/build-canaries/nevergreen"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.5.1"]
-                 [compojure "1.5.2"]
+                 [ring "1.6.0"]
+                 [compojure "1.6.0"]
                  [environ "1.1.0"]
                  [cheshire "5.7.1"]
                  [clj-cctray "0.11.0"]
                  [clj-http "3.5.0"]
                  [ring-curl "0.3.0"]
                  [ring/ring-json "0.4.0"]
-                 [ring/ring-defaults "0.2.3"]
+                 [ring/ring-defaults "0.3.0"]
                  [bk/ring-gzip "0.2.1"]
                  [ring-basic-authentication "1.0.5"]
                  [base64-clj "0.1.1"]
@@ -20,9 +20,7 @@
                  [org.slf4j/log4j-over-slf4j "1.7.25"]
                  [org.slf4j/jul-to-slf4j "1.7.25"]
                  [org.slf4j/jcl-over-slf4j "1.7.25"]]
-  :plugins [[lein-ring "0.11.0"]]
   :min-lein-version "2.0.0"
-  :ring {:handler nevergreen.app/all-routes :port 5000}
   :uberjar-name "nevergreen-standalone.jar"
   :main nevergreen.app
   :aot [nevergreen.app]
@@ -31,6 +29,7 @@
                    :dependencies [[midje "1.8.3"]
                                   [org.seleniumhq.selenium/selenium-support "3.4.0"]
                                   [org.seleniumhq.selenium/selenium-java "3.4.0" :exclusions [org.seleniumhq.selenium/selenium-support
+                                                                                              org.seleniumhq.selenium/selenium-api
                                                                                               org.eclipse.jetty/jetty-io
                                                                                               org.eclipse.jetty/jetty-util]]
                                   [clj-webdriver/clj-webdriver "0.7.2"]
