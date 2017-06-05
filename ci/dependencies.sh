7c6175825a52ee97dd9df1b668254c8738c5a2bb
Christopher Martin <eternalepoch@gmail.com>
2017-06-05 18:13:39 +0100

    manually install semver to try and fix CI build

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -5,5 +5,9 @@ npm install npm@latest --no-save
 npm prune
 npm install
 
+# TODO: temp workaround for npm@5 issues causing broken build on CI
+# seems to be related to https://github.com/npm/npm/issues/15558 maybe?
+npm install semver
+
 # download server dependencies
 ./lein.sh deps
