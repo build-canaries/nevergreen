64108b550014ff3654ec35f6056f047e2be2181d
Christopher Martin <eternalepoch@gmail.com>
2017-06-09 15:47:03 +0100

    [#177] remove manual install of semver

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -4,9 +4,5 @@
 npm install npm@latest -g
 npm install
 
-# TODO: since updating to npm@5 the build breaks with an error about missing semver, this is a temp workaround to keep the build working
-# see issue https://github.com/build-canaries/nevergreen/issues/177
-npm install semver@5.3.0
-
 # download server dependencies
 ./lein.sh deps
