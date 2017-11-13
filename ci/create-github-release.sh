70156223a29e8fe64006780c56160c812a15f547
Christopher Martin <eternalepoch@gmail.com>
2017-11-13 00:44:29 +0000

    [#190] upload the jar to the release

--- ci/create-github-release.sh
+++ ci/create-github-release.sh
@@ -6,9 +6,20 @@ body="### New Features\r\n\r\n### Closed Bugs\r\n\r\n### Improvements\r\n"
 
 echo "Creating a GitHub release with tag [${version}] and name [${versionName}]"
 
-curl -v \
-  -X POST \
+responseJson=$(curl -v \
   -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
   -H "Content-Type: application/vnd.github.v3+json" \
   -d "{\"tag_name\": \"v${version}\", \"target_commitish\": \"master\", \"name\": \"${versionName}\", \"body\": \"${body}\", \"draft\": true, \"prerelease\": false}" \
-  https://api.github.com/repos/build-canaries/nevergreen/releases
+  https://api.github.com/repos/build-canaries/nevergreen/releases)
+
+echo "Got response [${responseJson}]"
+
+uploadUrl=$(echo ${responseJson} | jq -r '.upload_url' | sed "s|{?name,label}||")
+
+echo "Adding the nevergreen-standalone.jar as an asset using URL [${uploadUrl}]"
+
+curl -v \
+  -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
+  -H "Content-Type: application/zip" \
+  --data-binary '@./target/nevergreen-standalone.jar' \
+  "${uploadUrl}?name=nevergreen-standalone.jar"
