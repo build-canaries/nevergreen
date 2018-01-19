0917fafceb3e109b02255a98d7509f3cc7162c7e
Christopher Martin <eternalepoch@gmail.com>
2018-01-19 00:22:34 +0000

    [#186] attempts to fix test reports

--- ci/test.sh
+++ ci/test.sh
@@ -7,3 +7,9 @@ npm run coverage
 echo "running the server tests"
 JVM_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=off" ./lein.sh eastwood # linting
 JVM_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=off" ./lein.sh midje 'nevergreen.*' # unit tests
+
+echo "moving test reports"
+mkdir -p ./target/test-reports/client
+mv ./target/client/*.xml ./target/test-reports/client
+mkdir -p ./target/test-reports/server
+mv ./target/surefire-reports/*.xml ./target/test-reports/server
