d4ff79a8b883cefd15ade0ec4135ec3a51f353c3
Christopher Martin <eternalepoch@gmail.com>
2018-01-22 15:46:35 +0000

    removes verbose logging

--- ci/release-github.sh
+++ ci/release-github.sh
@@ -4,7 +4,7 @@ body="### New Features\r\n\r\n### Closed Bugs\r\n\r\n### Improvements\r\n"
 
 echo "Creating a GitHub release with tag [${VERSION}] and name [${VERSION_NAME}]"
 
-responseJson=$(curl -v \
+responseJson=$(curl \
   -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
   -H "Content-Type: application/vnd.github.v3+json" \
   -d "{\"tag_name\": \"v${VERSION}\", \"target_commitish\": \"master\", \"name\": \"${VERSION_NAME}\", \"body\": \"${body}\", \"draft\": true, \"prerelease\": false}" \
@@ -16,7 +16,7 @@ uploadUrl=$(echo ${responseJson} | jq -r '.upload_url' | sed "s|{?name,label}||"
 
 echo "Adding the nevergreen-standalone.jar as an asset using URL [${uploadUrl}]"
 
-curl -v \
+curl \
   -u ${GITHUB_USERNAME}:${GITHUB_TOKEN} \
   -H "Content-Type: application/zip" \
   --data-binary '@./target/nevergreen-standalone.jar' \
