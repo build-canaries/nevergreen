0d324da8055853f35e289c7bd1814f0c53e286ad
Christopher Martin <eternalepoch@gmail.com>
2018-01-22 01:04:28 +0000

    [#186] try and make using nvm more explicit

--- ci/install-node.sh
+++ ci/install-node.sh
@@ -4,7 +4,8 @@ echo "downloading nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 echo "sourcing '$HOME/.nvm/nvm.sh'"
-. ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 echo "installing node"
 nvm install
