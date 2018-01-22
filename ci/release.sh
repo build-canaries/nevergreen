09221ebc648c7dc12e64b88ee698bd4abb61a2d2
Joe Wright <joe@joejag.com>
2018-01-22 19:08:57 +0000

    Update service name

--- ci/release.sh
+++ ci/release.sh
@@ -2,5 +2,5 @@
 
 echo "deploying to production"
 scp ./target/nevergreen-standalone.jar nevergreen@nevergreen.io:/home/nevergreen/deploy/production
-ssh nevergreen@nevergreen.io "sudo service nevergreen restart"
+ssh nevergreen@nevergreen.io "sudo service nevergreen-production restart"
 ssh nevergreen@nevergreen.io "sudo service nevergreen-failover restart"
