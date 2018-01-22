5ce8ffe4e6165fa7980af9d9eeeffd49db53cc59
Christopher Martin <eternalepoch@gmail.com>
2018-01-22 01:30:57 +0000

    [#186] the nvm use shouldn't be required as node appears to have been added to the path correctly...

--- ci/install-node.sh
+++ ci/install-node.sh
@@ -3,7 +3,7 @@
 echo "downloading nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-echo "sourcing '$HOME/.nvm/nvm.sh'"
+echo "sourcing nvm"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
