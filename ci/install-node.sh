e240c0af159293f2dad28aabb7340d20e43b68fa
Christopher Martin <eternalepoch@gmail.com>
2018-01-22 00:15:20 +0000

    [#186] try and fix permission denied error

--- ci/install-node.sh
+++ ci/install-node.sh
@@ -4,10 +4,10 @@ echo "downloading nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
 echo "sourcing '$HOME/.nvm/nvm.sh'"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+. ~/.nvm/nvm.sh
 
 echo "installing node"
 nvm install
 
-echo "$NVM_DIR/nvm.sh" >> $BASH_ENV
+echo "export NVM_DIR=\"~/.nvm\"" >> $BASH_ENV
+echo "~/.nvm/nvm.sh" >> $BASH_ENV
