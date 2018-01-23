098e598a890795cf84c1a25e5b64fe6df60f7e8b
Christopher Martin <eternalepoch@gmail.com>
2018-01-23 22:45:35 +0000

    update with CSP2 values

--- src/nevergreen/wrap_content_security_policy.clj
+++ src/nevergreen/wrap_content_security_policy.clj
@@ -4,14 +4,16 @@
 
 (def ^:private default-src "default-src 'self'")
 (def ^:private script-src "script-src 'self' 'unsafe-eval'")
+(def ^:private worker-src "worker-src 'self'")
 (def ^:private style-src "style-src 'self' 'unsafe-inline'")
 (def ^:private img-src "img-src * data:")
 (def ^:private font-src "font-src 'self' data:")
 (def ^:private media-src "media-src *")
 (def ^:private connect-src "connect-src 'self' https://api.github.com https://gist.githubusercontent.com")
 (def ^:private object-src "object-src 'none'")
-(def ^:private child-src "child-src 'none'")
-(def ^:private sources [default-src script-src style-src img-src font-src media-src connect-src object-src child-src])
+(def ^:private child-src "child-src 'self'")
+(def ^:private frame-src "frame-src 'none'")
+(def ^:private sources [default-src script-src worker-src style-src img-src font-src media-src connect-src object-src child-src frame-src])
 
 (defn wrap-content-security-policy [app]
   (fn [req]
