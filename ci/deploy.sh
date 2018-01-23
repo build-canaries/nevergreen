31a25229ca40fd97347bd3ee1d7de0f3f826e18b
Joe Wright <joe@joejag.com>
2018-01-23 00:49:45 +0000

    fix restart staging script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,4 +2,4 @@
 
 echo "deploying to staging"
 scp ./target/nevergreen-standalone.jar nevergreen@35.176.150.70:/home/nevergreen/deploy/staging
-ssh nevergreen@35.176.150.70 "sudo /bin/systemctl * nevergreen-staging-1"
+ssh nevergreen@35.176.150.70 "sudo /bin/systemctl restart nevergreen-staging-1"
