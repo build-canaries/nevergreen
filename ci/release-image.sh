b1bc00ad17d480ff3ec03da5dfab237d8f8b3180
Manasi Kulkarni <mkulkarn@thoughtworks.com>
2018-01-30 16:30:49 +0000

    [#203] Reverts previous commits and adds some temp logs to help debug the problem

--- ci/release-image.sh
+++ ci/release-image.sh
@@ -6,6 +6,9 @@ docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 
 echo "Creating docker image with tag [${VERSION}]"
 
+echo "what is your context currently???????????"
+pwd
+
 docker build --build-arg VERSION=${VERSION} -f ~/nevergreen/docker/Dockerfile -t buildcanariesteam/nevergreen:latest -t buildcanariesteam/nevergreen:${VERSION} ~/nevergreen 
 
 docker push buildcanariesteam/nevergreen:${VERSION} buildcanariesteam/nevergreen:latest
