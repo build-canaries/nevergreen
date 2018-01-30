6ee3cff88d1c07e43247b462f0a249e26688388f
Manasi Kulkarni <mkulkarn@thoughtworks.com>
2018-01-30 21:40:34 +0000

    [#203] Removes debug logs

--- ci/release-image.sh
+++ ci/release-image.sh
@@ -6,15 +6,6 @@ docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 
 echo "Creating docker image with tag [${VERSION}]"
 
-echo "docker version is "
-docker --version
-
-echo "what is you in your dir???????????"
-ls
-
-echo "what permissions do you have???????????"
-ls -la Dockerfile
-
 docker build --build-arg VERSION=${VERSION} -t buildcanariesteam/nevergreen:latest -t buildcanariesteam/nevergreen:${VERSION} .
 
 docker push buildcanariesteam/nevergreen:${VERSION} buildcanariesteam/nevergreen:latest
