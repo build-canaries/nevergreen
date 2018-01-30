88a5970e7f05fa72f1bbf675710d0d4fcec02d37
Manasi Kulkarni <mkulkarn@thoughtworks.com>
2018-01-30 20:55:44 +0000

    [#203] some logs around container info

--- ci/release-image.sh
+++ ci/release-image.sh
@@ -7,7 +7,10 @@ docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}
 echo "Creating docker image with tag [${VERSION}]"
 
 echo "what is your context currently???????????"
-ls
+ls -la Dockerfile
+
+echo "docker version is "
+docker --version
 
 docker build --build-arg VERSION=${VERSION} -t buildcanariesteam/nevergreen:latest -t buildcanariesteam/nevergreen:${VERSION} .
 
