f9851c7ed55e140447fb35466c245ebf0efce189
Christopher Martin <eternalepoch@gmail.com>
2018-04-11 21:34:57 +0100

    [#221] switch logging to use logback
    This gives people self hosting more options to configure logging

--- src/nevergreen/app.clj
+++ src/nevergreen/app.clj
@@ -1,5 +1,6 @@
 (ns nevergreen.app
-  (:import org.joda.time.DateTime)
+  (:import org.joda.time.DateTime
+           org.slf4j.bridge.SLF4JBridgeHandler)
   (:require [compojure.core :refer :all]
             [ring.adapter.jetty :refer [run-jetty]]
             [cheshire.generate :as cheshire]
@@ -7,6 +8,9 @@
             [nevergreen.api.routes :refer :all]
             [nevergreen.app.routes :refer :all])
   (:gen-class))
+
+(SLF4JBridgeHandler/removeHandlersForRootLogger)
+(SLF4JBridgeHandler/install)
 
 (cheshire/add-encoder DateTime (fn [date json-generator]
                                  (.writeString json-generator (.toString date))))
