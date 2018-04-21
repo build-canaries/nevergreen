3787aa5398479b9e3d01c1f64c2857af8c9f3dcf
Christopher Martin <eternalepoch@gmail.com>
2018-04-21 23:47:57 +0100

    handle async calls in Timer
    This makes sure the monitor page only triggers one call to the
    server at a time, in the case of a small refresh time and a
    slow connection.

--- src/client/common/Timer.js
+++ src/client/common/Timer.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types'
 import {debug} from './Logger'
 
 class Timer extends Component {
+  createTimeout = () => {
+    this.timeoutId = setTimeout(this.run, this.props.interval * 1000)
+    debug(`created timeout [${this.timeoutId}] to run in [${this.props.interval}s]`)
+  }
+
+  run = () => {
+    return Promise.resolve(this.props.onTrigger())
+      .then(this.createTimeout)
+  }
+
   componentDidMount() {
-    const run = () => {
-      this.timeoutId = setTimeout(run, this.props.interval * 1000)
-      debug(`created timeout [${this.timeoutId}] to run in [${this.props.interval}s]`)
-      this.props.onTrigger()
-    }
-    run()
+    this.run()
   }
 
   componentWillUnmount() {
