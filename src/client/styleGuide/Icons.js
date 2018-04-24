f38a7ef602942342cc53bd99aa1cb82e126c9b7d
Christopher Martin <eternalepoch@gmail.com>
2018-04-24 20:23:28 +0100

    [#215] adds an example grid

--- src/client/styleGuide/Icons.js
+++ src/client/styleGuide/Icons.js
@@ -1,42 +1,45 @@
 import React, {Component} from 'react'
 import icons from '../common/fonts/icon-font.scss'
 import StyleGuideSection from './StyleGuideSection'
+import styles from './style-guide.scss'
 
 class Icons extends Component {
   render() {
     return (
-      <StyleGuideSection title='Icons'>
-        <div className={icons['icon-bin']}>bin</div>
-        <div className={icons['icon-checkbox-checked']}>checkbox checked</div>
-        <div className={icons['icon-checkbox-unchecked']}>checkbox unchecked</div>
-        <div className={icons['icon-checkmark']}>checkmark</div>
-        <div className={icons['icon-circle']}>circle</div>
-        <div className={icons['icon-circle-down']}>circle down</div>
-        <div className={icons['icon-circle-up']}>circle up</div>
-        <div className={icons['icon-cloud-download']}>cloud download</div>
-        <div className={icons['icon-cloud-upload']}>cloud upload</div>
-        <div className={icons['icon-cog']}>cog</div>
-        <div className={icons['icon-cross']}>cross</div>
-        <div className={icons['icon-dice']}>dice</div>
-        <div className={icons['icon-display']}>display</div>
-        <div className={icons['icon-eye']}>eye</div>
-        <div className={icons['icon-eye-blocked']}>eye blocked</div>
-        <div className={icons['icon-floppy-disk']}>floppy disk</div>
-        <div className={icons['icon-github4']}>github 4</div>
-        <div className={icons['icon-hipster']}>hipster</div>
-        <div className={icons['icon-list']}>list</div>
-        <div className={icons['icon-lock']}>lock</div>
-        <div className={icons['icon-loop2']}>loop 2</div>
-        <div className={icons['icon-menu3']}>menu 3</div>
-        <div className={icons['icon-menu4']}>menu 4</div>
-        <div className={icons['icon-notification']}>notification</div>
-        <div className={icons['icon-paste']}>paste</div>
-        <div className={icons['icon-play3']}>play 3</div>
-        <div className={icons['icon-plus']}>plus</div>
-        <div className={icons['icon-question']}>question</div>
-        <div className={icons['icon-stop2']}>stop 2</div>
-        <div className={icons['icon-twitter3']}>twitter 3</div>
-        <div className={icons['icon-unlocked']}>unlocked</div>
+      <StyleGuideSection title='Icons (in a Responsive Grid)'>
+        <div className={styles.icons}>
+          <span className={icons['icon-bin']}>bin</span>
+          <span className={icons['icon-checkbox-checked']}>checkbox checked</span>
+          <span className={icons['icon-checkbox-unchecked']}>checkbox unchecked</span>
+          <span className={icons['icon-checkmark']}>checkmark</span>
+          <span className={icons['icon-circle']}>circle</span>
+          <span className={icons['icon-circle-down']}>circle down</span>
+          <span className={icons['icon-circle-up']}>circle up</span>
+          <span className={icons['icon-cloud-download']}>cloud download</span>
+          <span className={icons['icon-cloud-upload']}>cloud upload</span>
+          <span className={icons['icon-cog']}>cog</span>
+          <span className={icons['icon-cross']}>cross</span>
+          <span className={icons['icon-dice']}>dice</span>
+          <span className={icons['icon-display']}>display</span>
+          <span className={icons['icon-eye']}>eye</span>
+          <span className={icons['icon-eye-blocked']}>eye blocked</span>
+          <span className={icons['icon-floppy-disk']}>floppy disk</span>
+          <span className={icons['icon-github4']}>github 4</span>
+          <span className={icons['icon-hipster']}>hipster</span>
+          <span className={icons['icon-list']}>list</span>
+          <span className={icons['icon-lock']}>lock</span>
+          <span className={icons['icon-loop2']}>loop 2</span>
+          <span className={icons['icon-menu3']}>menu 3</span>
+          <span className={icons['icon-menu4']}>menu 4</span>
+          <span className={icons['icon-notification']}>notification</span>
+          <span className={icons['icon-paste']}>paste</span>
+          <span className={icons['icon-play3']}>play 3</span>
+          <span className={icons['icon-plus']}>plus</span>
+          <span className={icons['icon-question']}>question</span>
+          <span className={icons['icon-stop2']}>stop 2</span>
+          <span className={icons['icon-twitter3']}>twitter 3</span>
+          <span className={icons['icon-unlocked']}>unlocked</span>
+        </div>
       </StyleGuideSection>
     )
   }
