d71160e77ae131b45c90c8c4dc3e275e9f54694a
Christopher Martin <eternalepoch@gmail.com>
2018-06-05 22:47:32 +0100

    fixes configuration getting cleared on reload

--- src/client/common/repo/Migrations.js
+++ src/client/common/repo/Migrations.js
@@ -1,9 +1,3 @@
-import {migrate as ZeroPointElevenPointZero} from './migrations/0.11.0'
-
-const migrations = [ZeroPointElevenPointZero]
-
 export function migrate(data) {
-  return migrations.reduce((reduction, migration) => {
-    return migration(reduction)
-  }, data)
+  return data
 }
