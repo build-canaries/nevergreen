607d0b271b758b44d88aca0f4b532dcf66503bc0
Christopher Martin <eternalepoch@gmail.com>
2018-09-13 22:39:22 +0100

    make the referrer policy more secure

--- src/nevergreen/wrap_referrer_policy.clj
+++ src/nevergreen/wrap_referrer_policy.clj
@@ -4,4 +4,4 @@
 (defn wrap-referrer-policy [app]
   (fn [req]
     (let [res (app req)]
-      (assoc-in res [:headers "Referrer-Policy"] "origin-when-cross-origin, strict-origin-when-cross-origin"))))
+      (assoc-in res [:headers "Referrer-Policy"] "strict-origin-when-cross-origin"))))
