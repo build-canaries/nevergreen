bdff0b17752d1f930634353803b3c5c115e3291f
Ste <cowley05@hotmail.co.uk>
2018-12-01 20:29:42 +0000

    [#162] Adds username and password to cypress tests in ci
    Co-authored-by: Joe Wright <joe@joejag.com>
    Co-authored-by: Christopher Martin <eternalepoch@gmail.com>

--- ci/export-functional-test-variables.sh
+++ ci/export-functional-test-variables.sh
@@ -8,6 +8,6 @@ versionMeta=$(cat ${metaFile})
 
 echo "export CYPRESS_BASE_URL=\"https://staging.nevergreen.io\"" >> $BASH_ENV
 echo "export CYPRESS_TRAY_URL=\"https://drive.google.com/uc?export=download&id=0BzdMs1jfanaARkJJX1VRR2QtdTA\"" >> $BASH_ENV
-echo "export CYPRESS_TRAY_USERNAME=\"\"" >> $BASH_ENV
-echo "export CYPRESS_TRAY_PASSWORD=\"\"" >> $BASH_ENV
+echo "export CYPRESS_TRAY_USERNAME=\"u\"" >> $BASH_ENV
+echo "export CYPRESS_TRAY_PASSWORD=\"p\"" >> $BASH_ENV
 echo "export CYPRESS_FULL_VERSION=\"$version+$versionMeta\"" >> $BASH_ENV
