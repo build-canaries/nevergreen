a1344679dd5fdebc45e63c8c440dd66496ea9955
Ste <cowley05@hotmail.co.uk>
2018-12-01 20:10:59 +0000

    [#162] use the circle ci variables directly

--- ci/update-version.sh
+++ ci/update-version.sh
@@ -2,5 +2,8 @@
 
 metaFile="./resources/version_meta.txt"
 
+BUILD_NUM=$CIRCLE_BUILD_NUM
+COMMIT_HASH=${CIRCLE_SHA1:0:7}
+
 echo "updating version meta with commit hash [$COMMIT_HASH] and build number [$BUILD_NUM] in file [$metaFile] ..."
 echo -n "$BUILD_NUM.$COMMIT_HASH" > ${metaFile}
