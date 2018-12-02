21fd54571feebf9e02578d3fdb0872e443dabda8
Joe Wright <joe@joejag.com>
2018-12-02 17:22:11 +0000

    Separate webpack configs and enable quicker watcher

---
+++ webpack/dev.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const baseConfig = require('./base.config.js')
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html'
+    })
+  ]
+})
---
+++ webpack/prod.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const { GenerateSW } = require('workbox-webpack-plugin')
+
+const baseConfig = require('./base.config.js')
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/client/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    }),
+    new OptimizeCssAssetsPlugin(),
+    new GenerateSW({
+      swDest: 'service-worker.js',
+      cacheId: 'nevergreen',
+      exclude: [/\.map$/, /asset-manifest\.json$/],
+      importWorkboxFrom: 'local',
+      clientsClaim: true,
+      skipWaiting: true,
+      navigateFallback: '/index.html',
+      navigateFallbackBlacklist: [
+        new RegExp('/[^/]+\\.[^/]+$') // Exclude URLs containing a dot, as they're likely a resource in public
+      ]
+    })
+  ]
+})
