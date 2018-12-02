aa201825ae51babf937e1fda20fc44de3ecdf714
Joe Wright <joe@joejag.com>
2018-12-02 18:22:09 +0000

    [#162] Add commands to Cypress to clean up test

--- cypress/integration/journey_spec.js
+++ cypress/integration/journey_spec.js
@@ -1,92 +1,73 @@
-/*global describe,beforeEach,it,cy,Cypress */
+/*global describe,before,it,cy,Cypress */
 
 describe('Journey', function () {
 
-  beforeEach(() => {
-    if (window.navigator && navigator.serviceWorker) {
-      navigator.serviceWorker.getRegistrations()
-        .then((registrations) => {
-          registrations.forEach((registration) => {
-            registration.unregister()
-          })
-        })
-    }
-
-
+  before(function () {
+    cy.clearIndexDb()
+    cy.unregisterServiceWorkers()
   })
 
-  function locate(thing) {
-    return `[data-locator=${thing}]`
-  }
-
-  it('do', function () {
-    Cypress.on('window:before:load', (win) => {
-      win.indexedDB.deleteDatabase('nevergreen')
-    })
-
+  it('should all work fine', function () {
     cy.visit('/')
-
     cy.location('pathname').should('include', 'tracking')
 
-    cy.get(locate('add-tray-url')).type(Cypress.env('TRAY_URL'))
-
+    cy.locate('add-tray-url').type(Cypress.env('TRAY_URL'))
     if(Cypress.env('TRAY_USERNAME') && Cypress.env('TRAY_PASSWORD')) {
-      cy.get(locate('add-tray-username')).type(Cypress.env('TRAY_USERNAME'))
-      cy.get(locate('add-tray-password')).type(Cypress.env('TRAY_PASSWORD'))
+      cy.locate('add-tray-username').type(Cypress.env('TRAY_USERNAME'))
+      cy.locate('add-tray-password').type(Cypress.env('TRAY_PASSWORD'))
     }
+    cy.locate('add-tray').click()
 
-    cy.get(locate('add-tray')).click()
+    cy.locate('tray')
+      .locate('container-sub-title').contains(Cypress.env('TRAY_URL'))
+      .locate('exclude-all').click()
+      .locate('include-all').click()
+      .locate('tab-settings').click()
+      .locate('generate-random').click()
+      .locate('tray-name').clear().type('renamed tray').blur()
+      .locate('container-title').contains(/^renamed tray$/)
+      .locate('tab-projects').click()
+      .locate('available-projects-list').contains('failure building project')
+      .locate('available-projects-list').contains('failure sleeping project')
+      .locate('available-projects-list').contains('success building project')
+      .locate('available-projects-list').contains('success sleeping project')
 
-    cy.get(locate('tray'))
-      .get(locate('container-sub-title')).contains(Cypress.env('TRAY_URL'))
-      .get(locate('exclude-all')).click()
-      .get(locate('include-all')).click()
-      .get(locate('tab-settings')).click()
-      .get(locate('generate-random')).click()
-      .get(locate('tray-name')).clear().type('renamed tray').blur()
-      .get(locate('container-title')).contains(/^renamed tray$/)
-      .get(locate('tab-projects')).click()
-      .get(locate('available-projects-list')).contains('failure building project')
-      .get(locate('available-projects-list')).contains('failure sleeping project')
-      .get(locate('available-projects-list')).contains('success building project')
-      .get(locate('available-projects-list')).contains('success sleeping project')
-
-    cy.get(locate('menu-success')).click()
+    cy.locate('menu-success').click()
     cy.location('pathname').should('include', 'success')
-    cy.get(locate('message')).type('some message')
-    cy.get(locate('add-message')).click()
-    cy.get(locate('success-message')).contains('=(^.^)=')
-    cy.get(locate('success-message')).contains('some message')
+    cy.locate('message').type('some message')
+    cy.locate('add-message').click()
+    cy.locate('success-message').contains('=(^.^)=')
+    cy.locate('success-message').contains('some message')
 
-    cy.get(locate('message'))
+    cy.locate('message')
       .type('https://raw.githubusercontent.com/build-canaries/nevergreen/master/doc/screenshot.png')
-    cy.get(locate('add-message')).click()
-    cy.get(locate('success-image')).should('be.visible')
+    cy.locate('add-message').click()
+    cy.locate('success-image').should('be.visible')
 
     // settings
-    cy.get(locate('menu-settings')).click()
+    cy.locate('menu-settings').click()
     cy.location('pathname').should('include', 'settings')
 
-    cy.get(locate('play-sounds')).click()
-    cy.get(locate('refresh-time')).select('60')
-    cy.get(locate('click-to-show-menu')).click()
+    cy.locate('play-sounds').click()
+    cy.locate('refresh-time').select('60')
+    cy.locate('click-to-show-menu').click()
 
-    cy.get(locate('show-tray-names')).check()
-    cy.get(locate('show-build-times')).check()
-    cy.get(locate('show-broken-build-times')).check()
-    cy.get(locate('show-build-labels')).check()
-    cy.get(locate('max-projects-to-show')).select('6')
-    cy.get(locate('build-label')).should('exist')
-    cy.get(locate('tray-name')).should('exist')
-    cy.get(locate('duration')).should('exist') // todo: building vs broken time
+    cy.locate('show-tray-names').check()
+    cy.locate('show-build-times').check()
+    cy.locate('show-broken-build-times').check()
+    cy.locate('show-build-labels').check()
+    cy.locate('max-projects-to-show').select('6')
+    cy.locate('build-label').should('exist')
+    cy.locate('tray-name').should('exist')
+    cy.locate('duration').should('exist') // todo: building vs broken time
 
-    cy.get(locate('show-tray-names')).uncheck()
-    cy.get(locate('show-build-times')).uncheck()
-    cy.get(locate('show-broken-build-times')).uncheck()
-    cy.get(locate('show-build-labels')).uncheck()
-    cy.get(locate('max-projects-to-show')).select('30')
-    cy.get(locate('build-label')).should('not.exist')
-    cy.get(locate('tray-name')).should('not.exist')
-    cy.get(locate('duration')).should('not.exist')
+    cy.locate('show-tray-names').uncheck()
+    cy.locate('show-build-times').uncheck()
+    cy.locate('show-broken-build-times').uncheck()
+    cy.locate('show-build-labels').uncheck()
+    cy.locate('max-projects-to-show').select('30')
+    cy.locate('build-label').should('not.exist')
+    cy.locate('tray-name').should('not.exist')
+    cy.locate('duration').should('not.exist')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+/*global cy,Cypress */
+
+// Enable looking up elements by the 'data-locater' attribute without in a concise way
+Cypress.Commands.add('locate', (thing) => {
+  cy.get(`[data-locator=${thing}]`)
+})
+
+// nuke previous data from test runs
+Cypress.Commands.add('clearIndexDb', () => {
+  Cypress.on('window:before:load', (win) => {
+    win.indexedDB.deleteDatabase('nevergreen')
+  })
+})
+
+Cypress.Commands.add('unregisterServiceWorkers', () => {
+  if (window.navigator && navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => { registration.unregister() })
+    })
+  }
+})
