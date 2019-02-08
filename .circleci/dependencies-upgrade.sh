d7a3a508d414224959aa3bdf92a5a699f9915cbc
Joe Wright <joe@joejag.com>
2019-02-08 23:24:43 +0000

    upgrade script: only run tests if changes have happened

--- .circleci/dependencies-upgrade.sh
+++ .circleci/dependencies-upgrade.sh
@@ -7,14 +7,15 @@ echo "updating client dependencies"
 npm install -g npm-check-updates
 npx ncu -u
 
-echo "checking things still work"
-./pre-push.sh
+if [[ `git status --porcelain` ]]; then
+  echo "checking things still work"
+  ./pre-push.sh
 
-TYPE_OF_RUN=${1:-testing} 
-if [ $TYPE_OF_RUN == 'push' ]
-  then
-  echo "pushing changes"
-  git add -a .
-  git commit -m 'Updating dependencies'
-  git push
+  TYPE_OF_RUN=${1:-testing} 
+  if [ $TYPE_OF_RUN == 'push' ]; then
+    echo "pushing changes"
+    git add -a .
+    git commit -m 'Updating dependencies'
+    git push
+  fi
 fi
