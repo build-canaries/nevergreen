249e3b7b7c5d9a364a6913710d773ee79bb145d6
Christopher Martin <eternalepoch@gmail.com>
2019-03-06 20:51:44 +0000

    [#274] removes some unused (transitive) dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.7.1" :exclusions [org.eclipse.jetty/jetty-server]]
                  [org.eclipse.jetty/jetty-server "9.4.15.v20190215"]
-                 [compojure "1.6.1"]
+                 [compojure "1.6.1" :exclusions [ring/ring-codec]]
                  [environ "1.1.0"]
                  [cheshire "5.8.1"]
                  [clj-cctray "1.0.2"]
@@ -16,8 +16,8 @@
                  [ring-basic-authentication "1.0.5"]
                  [base64-clj "0.1.1"]
                  [camel-snake-kebab "0.4.0"]
-                 [com.cemerick/url "0.1.1"]
-                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
+                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/log4j-over-slf4j "1.7.26"]
                  [org.slf4j/jul-to-slf4j "1.7.26"]
                  [org.slf4j/jcl-over-slf4j "1.7.26"]]
@@ -28,11 +28,13 @@
   :javac-options ["-Dclojure.compiler.direct-linking=true"]
   :aliases {"lint"     ["with-profile" "+test" "eastwood"]
             "coverage" ["with-profile" "+test" "cloverage"]}
-  :profiles {:dev  {:plugins      [[lein-ancient "0.6.15"]
-                                   [jonase/eastwood "0.3.4"]
-                                   [lein-cloverage "1.1.0"]
-                                   [com.livingsocial/lein-dependency-check "1.0.4"]]
-                    :dependencies [[ring/ring-mock "0.3.2"]]}
+  :profiles {:dev  {:plugins [[lein-ancient "0.6.15"]
+                              [jonase/eastwood "0.3.5"]
+                              [lein-cloverage "1.1.0"]
+                              [com.livingsocial/lein-dependency-check "1.1.2"]]}
              :test {:jvm-opts ["-Dlogback.configurationFile=./test/logback-unit.xml"]}}
-  :cloverage {:output "target/coverage-reports/server"
-              :junit? true})
+  :cloverage {:output           "target/coverage-reports/server"
+              :junit?           true
+              :ns-exclude-regex [#"nevergreen\.logging"]}
+  :dependency-check {:output-directory "target/security-reports/server"
+                     :suppression-file "config/nvd-suppressions.xml"})
