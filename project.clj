131555f2762f6993ff1a53f4a0711e08c42d338f
Christopher Martin <eternalepoch@gmail.com>
2019-06-30 22:00:37 +0100

    updates server deps

--- project.clj
+++ project.clj
@@ -30,7 +30,7 @@
             "coverage"      ["with-profile" "+test" "cloverage"]
             "check-updates" ["ancient" ":all"]}
   :profiles {:dev  {:plugins [[lein-ancient "0.6.15"]
-                              [jonase/eastwood "0.3.5"]
+                              [jonase/eastwood "0.3.6"]
                               [lein-cloverage "1.1.1"]
                               [lein-nvd "1.1.1"]]}
              :test {:jvm-opts ["-Dlogback.configurationFile=./test/logback-unit.xml"]}}
