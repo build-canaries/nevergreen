6b24f2d9e93e0eec46fd5ab8a9c36a7e2e62816a
Christopher Martin <eternalepoch@gmail.com>
2019-08-15 20:28:19 +0100

    updates server deps

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://github.com/build-canaries/nevergreen"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.7.1" :exclusions [org.eclipse.jetty/jetty-server]]
-                 [org.eclipse.jetty/jetty-server "9.4.19.v20190610"]
+                 [org.eclipse.jetty/jetty-server "9.4.20.v20190813"]
                  [compojure "1.6.1" :exclusions [ring/ring-codec]]
                  [environ "1.1.0"]
-                 [cheshire "5.8.1"]
+                 [cheshire "5.9.0"]
                  [clj-cctray "2.0.0"]
                  [clj-http "3.10.0"]
                  [ring-curl "1.0.1"]
-                 [ring/ring-json "0.4.0"]
+                 [ring/ring-json "0.5.0"]
                  [ring/ring-defaults "0.3.2"]
                  [bk/ring-gzip "0.3.0"]
                  [ring-basic-authentication "1.0.5"]
@@ -18,9 +18,9 @@
                  [camel-snake-kebab "0.4.0"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]
-                 [org.slf4j/jul-to-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.28"]
+                 [org.slf4j/jul-to-slf4j "1.7.28"]
+                 [org.slf4j/jcl-over-slf4j "1.7.28"]]
   :min-lein-version "2.0.0"
   :uberjar-name "nevergreen-standalone.jar"
   :main nevergreen.app
