12e863f4d42801daf12943a94bff5c368820c9d1
Prateek Jain <prateek.jain23995@yahoo.com>
2019-10-07 15:42:43 +0530

    Fetching tray asynchonously and cancelling the request after a timeout of 1 minute

--- src/nevergreen/gateway.clj
+++ src/nevergreen/gateway.clj
@@ -4,22 +4,23 @@
             [clojure.string :as s]
             [cemerick.url :as u])
   (:import (java.net UnknownHostException URISyntaxException ConnectException SocketTimeoutException)
-           (clojure.lang ExceptionInfo)))
+           (clojure.lang ExceptionInfo)
+           (java.util.concurrent TimeUnit TimeoutException ExecutionException)))
 
 (def ^:const ten-seconds 10000)
 (def ^:const redacted "REDACTED")
 
 (defn ^:dynamic client-get [url data]
-  (client/get url data))
+  (client/get url data (fn [_]) (fn [_])))
 
 (defn ^:dynamic client-post [url data]
-  (client/post url data))
+  (client/post url data (fn [_]) (fn [_])))
 
 (defn ^:dynamic client-patch [url data]
-  (client/patch url data))
+  (client/patch url data (fn [_]) (fn [_])))
 
 (defn ^:dynamic client-put [url data]
-  (client/put url data))
+  (client/put url data (fn [_]) (fn [_])))
 
 (defn- update-values [m f & args]
   (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
@@ -34,6 +35,10 @@
 (defn- handle-timeout [url redacted-url e]
   (log/info (str "[" redacted-url "] threw a SocketTimeoutException [" (.getMessage e) "]"))
   (throw (ex-info "Connection timeout" {:url url :status 504})))
+
+(defn- handle-client-timeout [url redacted-url e]
+  (log/info (str "[" redacted-url "] couldn't produce response within given time. Explicitly closing connection"))
+  (throw (ex-info "Deadline timeout" {:url url :status 504})))
 
 (defn- handle-unknown-host [url redacted-url e]
   (let [host (first (s/split (.getMessage e) #":"))
@@ -61,25 +66,28 @@
 (defn- http [url data method]
   (let [redacted-url (redact-url url)]
     (log/info (str "Calling [" redacted-url "]..."))
-    (try
-      (let [res (method url (merge {:insecure?             true
-                                    :socket-timeout        ten-seconds
-                                    :conn-timeout          ten-seconds
-                                    :throw-entire-message? true
-                                    :cookie-policy         :standard}
-                                   data))]
-        (log/info (str "[" redacted-url "] returned a status of [" (:status res) "]"))
-        (:body res))
-      (catch SocketTimeoutException e
-        (handle-timeout url redacted-url e))
-      (catch UnknownHostException e
-        (handle-unknown-host url redacted-url e))
-      (catch URISyntaxException e
-        (handle-invalid-url url redacted-url e))
-      (catch ConnectException e
-        (handle-connection-refused url redacted-url e))
-      (catch ExceptionInfo e
-        (handle-exception-info url redacted-url e)))))
+    (let [future (method url (merge {:insecure?             true
+                                     :socket-timeout        ten-seconds
+                                     :conn-timeout          ten-seconds
+                                     :throw-entire-message? true
+                                     :cookie-policy         :standard
+                                     :async                 true}
+                                    data))]
+      (try
+        (let [res (.get future 1 TimeUnit/MINUTES)]
+          (log/info (str "[" redacted-url "] returned a status of [" (.getStatusCode (.getStatusLine res)) "]"))
+          (.getContent (.getEntity res)))
+        (catch ExecutionException e
+          (let [original-exception (.getCause e)]
+            (condp instance? original-exception
+              UnknownHostException (handle-unknown-host url redacted-url original-exception)
+              URISyntaxException (handle-invalid-url url redacted-url original-exception)
+              ConnectException (handle-connection-refused url redacted-url original-exception)
+              ExceptionInfo (handle-exception-info url redacted-url original-exception)
+              SocketTimeoutException (handle-timeout url redacted-url original-exception))))
+        (catch TimeoutException e
+          (.cancel future true)
+          (handle-client-timeout url redacted-url e))))))
 
 (defn http-get [url data]
   (http url data client-get))
