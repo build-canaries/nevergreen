6453ce07573ae8e1f51b4e7a77f0f7536efb4e12
Prateek Jain <prateek.jain23995@yahoo.com>
2019-10-13 16:04:09 +0530

    Changing asyncronous implementation, now consuming ring response map instead of raw stream of data

--- test/nevergreen/gateway_test.clj
+++ test/nevergreen/gateway_test.clj
@@ -6,7 +6,8 @@
            (org.apache.http.message BasicHttpResponse BasicStatusLine)
            (org.apache.http HttpVersion)
            (org.apache.http.entity StringEntity)
-           (java.util.concurrent TimeoutException TimeUnit Future)))
+           (java.util.concurrent TimeoutException TimeUnit Future)
+           (java.io ByteArrayInputStream)))
 
 
 (defn- completed-future [result]
@@ -30,63 +31,70 @@
     (.failed basic-future exception)
     basic-future))
 
+(defn string->stream
+  ([s] (string->stream s "UTF-8"))
+  ([s encoding]
+   (-> s
+       (.getBytes encoding)
+       (ByteArrayInputStream.))))
+
 (deftest http-get
   (testing "adds additional data"
-    (binding [subject/client-get (fn [_ data]
+    (binding [subject/client-get (fn [_ data promise]
                                    (is (contains? data :headers))
                                    (is (contains? (:headers data) "Authentication"))
                                    (is (= "Basic some-password" (get (:headers data) "Authentication")))
-                                   (completed-future "result"))]
+                                   (deliver promise "result"))]
       (subject/http-get "http://some-url" {:headers {"Authentication" "Basic some-password"}})))
 
   (testing "returns the body"
-    (binding [subject/client-get (constantly (completed-future "some-body"))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (string->stream "some-body")))]
       (is (= "some-body" (slurp (subject/http-get "http://some-url" {}))))))
 
   (testing "uses the given url"
-    (binding [subject/client-get (fn [url _]
+    (binding [subject/client-get (fn [url _ promise]
                                    (is (= "http://some-url" url))
-                                   (completed-future "result"))]
+                                   (deliver promise (string->stream "result")))]
       (subject/http-get "http://some-url" {})))
 
   (testing "throws an error for unknown hosts"
-    (binding [subject/client-get (fn [_ _] (failed-future (UnknownHostException. "some-host: unknown error")))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (UnknownHostException. "some-host: unknown error")))]
       (is (thrown-with-msg? Exception
                             #"some-host is an unknown host, is the URL correct?"
                             (subject/http-get "http://some-host" {})))))
 
   (testing "throws an error for bad uri syntax"
-    (binding [subject/client-get (fn [_ _] (failed-future (URISyntaxException. "some-url" "Illegal character in authority at index 0")))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (URISyntaxException. "some-url" "Illegal character in authority at index 0")))]
       (is (thrown-with-msg? Exception
                             #"URL is invalid. Illegal character in authority at index 0: some-url"
                             (subject/http-get "http://some-url" {})))))
 
   (testing "throws an error for connect exception"
-    (binding [subject/client-get (fn [_ _] (failed-future (ConnectException. "Connection refused (Connection refused)")))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (ConnectException. "Connection refused (Connection refused)")))]
       (is (thrown-with-msg? Exception
                             #"Connection refused, is the URL correct?"
                             (subject/http-get "http://some-url" {})))))
 
   (testing "throws an error when the CI server responds with an error but blank reason phrase"
-    (binding [subject/client-get (fn [_ _] (failed-future (ex-info "irrelevant" {:reason-phrase "" :status 404})))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (ex-info "irrelevant" {:reason-phrase "" :status 404})))]
       (is (thrown-with-msg? Exception
                             #"Server returned a 404"
                             (subject/http-get "http://some-url" {})))))
 
   (testing "throws an error when the CI server responds with an error"
-    (binding [subject/client-get (fn [_ _] (failed-future (ex-info "irrelevant" {:reason-phrase "some-error"})))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (ex-info "irrelevant" {:reason-phrase "some-error"})))]
       (is (thrown-with-msg? Exception
                             #"Server returned a some-error"
                             (subject/http-get "http://some-url" {})))))
 
   (testing "throws an unknown error when the CI server responds with an error but no reason"
-    (binding [subject/client-get (fn [_ _] (failed-future (ex-info "irrelevant" {})))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (ex-info "irrelevant" {})))]
       (is (thrown-with-msg? Exception
                             #"Server returned a Unknown error"
                             (subject/http-get "http://some-url" {})))))
 
   (testing "throws an unknown error when SocketTimeoutException exception occurs"
-    (binding [subject/client-get (fn [_ _] (failed-future (SocketTimeoutException. "Timeout occurred")))]
+    (binding [subject/client-get (fn [_ _ promise] (deliver promise (SocketTimeoutException. "Timeout occurred")))]
       (is (thrown-with-msg? Exception
                             #"Connection timeout"
                             (subject/http-get "http://some-url" {})))))
@@ -94,15 +102,15 @@
   (testing "throws an an error when server did not respond in time and cancel the request"
     (let [call-count (atom 0)
           timeout-future (proxy [Future] []
-                   (get [timeout unit]
-                     (is (= 50 timeout))
-                     (is (= unit TimeUnit/SECONDS))
-                     (throw (TimeoutException.)))
-                   (cancel [interrupt]
-                     (is (= true interrupt))
-                     (swap! call-count inc)
-                     true))]
-      (binding [subject/client-get (fn [_ _] timeout-future)]
+                           (cancel [interrupt]
+                             (is (= true interrupt))
+                             (swap! call-count inc)
+                             true))]
+      (with-redefs [subject/get-result (fn [_ msec timeout-result]
+                                         (is (= 50000 msec))
+                                         (is (= :deadline-timeout timeout-result))
+                                         timeout-result)
+                    subject/client-get (constantly timeout-future)]
         (is (thrown-with-msg? Exception
                               #"Deadline timeout"
                               (subject/http-get "http://some-url" {})))
