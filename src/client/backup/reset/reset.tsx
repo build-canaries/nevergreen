41c09bbe590763e950e01496491982f60573e4da
Tharun <rajendrantharun@live.com>
2019-11-05 11:19:20 +0530

    Fixed PR review changes

--- src/client/backup/reset/reset.tsx
+++ src/client/backup/reset/reset.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import styles from './reset.scss'
+import { Container } from '../../common/Container'
+import {clear} from '../../configuration/LocalRepository'
 import {PrimaryButton} from '../../common/forms/Button'
 import {iBin} from '../../common/fonts/Icons'
-import {Messages, MessagesType} from '../../common/Messages'
+
 export function Reset() {
-    const messageType = MessagesType.ERROR
-    const messages = [ 'Reset your Nevergreen configuration back to defaults. Please note, resetting your configuration can not be undone!' ]
+    
     const resetConfiguration = () => {
-        window.localStorage.clear()
+        clear()
         window.location.reload()
     }
     return(
-        <div>
-        <Messages type={messageType}
-                messages={messages}
-                data-locator='messages'/>
-        <PrimaryButton className={styles.import}
+        <>
+        <Container title={'Reset Configuration'}>
+        <span className={styles.label}>Reset your Nevergreen configuration back to defaults. Please note, resetting your configuration can not be undone!</span>
+        <PrimaryButton className={styles.reset}
+                    id='reset-configuration'
                      onClick={resetConfiguration}
                      icon={iBin}
                      data-locator='reset'>
-        Reset Configuration
+        reset configuration
       </PrimaryButton>
-      </div>
+      </Container>
+    </>
     )
 }
