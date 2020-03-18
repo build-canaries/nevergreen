aa79d0613098d55b9d6686d3e4d46870f8370e1c
Christopher Martin <eternalepoch@gmail.com>
2020-03-18 20:39:46 +0000

    updates server deps

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
             "check-updates" ["ancient" ":all"]
             "audit"         ["nvd" "check"]}
   :profiles {:dev  {:plugins [[lein-ancient "0.6.15"]
-                              [jonase/eastwood "0.3.10"]
+                              [jonase/eastwood "0.3.11"]
                               [lein-cloverage "1.1.2"]
                               [lein-nvd "1.3.1"]]}
              :test {:jvm-opts ["-Dlogback.configurationFile=./test/logback-unit.xml"]}}
