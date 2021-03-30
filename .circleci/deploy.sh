8c5a62274152d769590a051c9c80d1b533292676
joe.wright <joe@joejag.com>
2021-03-30 01:08:02 +0100

    update serve ip address

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
 echo "deploying to staging"
-scp ./target/nevergreen-standalone.jar nevergreen@35.176.150.70:/home/nevergreen/deploy/staging
-ssh nevergreen@35.176.150.70 "sudo /bin/systemctl restart nevergreen-staging-1"
+scp ./target/nevergreen-standalone.jar nevergreen@35.176.75.186:/home/nevergreen/deploy/staging
+ssh nevergreen@35.176.75.186 "sudo /bin/systemctl restart nevergreen-staging-1"
--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
 echo "deploying to production"
-scp ./target/nevergreen-standalone.jar nevergreen@35.176.150.70:/home/nevergreen/deploy/production
-ssh nevergreen@35.176.150.70 "sudo /bin/systemctl restart nevergreen-production-1"
-ssh nevergreen@35.176.150.70 "sudo /bin/systemctl restart nevergreen-production-2"
+scp ./target/nevergreen-standalone.jar nevergreen@35.176.75.186:/home/nevergreen/deploy/production
+ssh nevergreen@35.176.75.186 "sudo /bin/systemctl restart nevergreen-production-1"
+ssh nevergreen@35.176.75.186 "sudo /bin/systemctl restart nevergreen-production-2"
