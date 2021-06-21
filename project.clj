3a3d60d4c6def53c5b6e02bda69a1a844c696917
Christopher Martin <eternalepoch@gmail.com>
2021-06-21 09:47:19 +0100

    updates server deps

--- project.clj
+++ project.clj
@@ -4,27 +4,25 @@
   ; use lein deps :tree to check dependency problems and actual versions used
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [ring "1.9.3" :exclusions [org.eclipse.jetty/jetty-server]]
-                 [org.eclipse.jetty/jetty-server "9.4.40.v20210413"]
+                 [org.eclipse.jetty/jetty-server "9.4.42.v20210604"]
                  [compojure "1.6.2" :exclusions [ring/ring-codec]]
                  [environ "1.2.0"]
                  [cheshire "5.10.0"]
                  [clj-cctray "2.0.0"]
                  ; commons-codec is excluded as ring pulls in a newer version
-                 ; commons-io 2.6 has CVE-2021-29425, so exclude and explicitly use fixed version
-                 [clj-http "3.12.1" :exclusions [commons-codec commons-io]]
-                 [commons-io "2.7"] ; check if this is still required if clj-http is upgraded
+                 [clj-http "3.12.2" :exclusions [commons-codec]]
                  [ring-curl "1.0.1"]
                  [ring/ring-json "0.5.1"]
-                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-defaults "0.3.3"]
                  [bk/ring-gzip "0.3.0"]
                  [ring-basic-authentication "1.1.0"]
                  [base64-clj "0.1.1"]
                  [camel-snake-kebab "0.4.2"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/log4j-over-slf4j "1.7.30"]
-                 [org.slf4j/jul-to-slf4j "1.7.30"]
-                 [org.slf4j/jcl-over-slf4j "1.7.30"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.31"]
+                 [org.slf4j/jul-to-slf4j "1.7.31"]
+                 [org.slf4j/jcl-over-slf4j "1.7.31"]]
   :min-lein-version "2.0.0"
   :uberjar-name "nevergreen-standalone.jar"
   :main nevergreen.app
@@ -39,9 +37,9 @@
             "check-updates" ["ancient" ":all"]
             "audit"         ["nvd" "check"]}
   :profiles {:dev  {:plugins [[lein-ancient "0.7.0"]
-                              [jonase/eastwood "0.4.2"]
+                              [jonase/eastwood "0.5.2"]
                               [lein-cloverage "1.2.2"]
-                              [lein-nvd "1.4.1"]
+                              [lein-nvd "1.5.0"]
                               [lein-eftest "0.5.9"]]}
              :test {:jvm-opts ["-Dlogback.configurationFile=./src/logback-tests.xml"]}}
   :cloverage {:output           "target/coverage-reports/server"
