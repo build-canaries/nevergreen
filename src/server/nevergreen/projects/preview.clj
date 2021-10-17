2b737257d9ae675f2bd13ed45c4ba3d0b8759da0
Christopher Martin <eternalepoch@gmail.com>
2021-10-17 17:45:11 +0100

    [#327] makes preview consistent

--- src/server/nevergreen/projects/preview.clj
+++ src/server/nevergreen/projects/preview.clj
@@ -1,42 +1,7 @@
 (ns nevergreen.projects.preview
   (:require [nevergreen.projects.projects :as projects]
-            [clojure.string :as s])
-  (:import (java.io StringReader)
-           (java.util Random)
-           (java.time Clock)))
-
-(defn- rand-str [^Random rnd]
-  (apply str (take (+ (.nextInt rnd 6) 4)
-                   (repeatedly #(char (+ (.nextInt rnd 26) 97))))))
-
-(defn- rand-prognosis [^Random rnd]
-  (condp = (.nextInt rnd 6)
-    0 :error
-    1 :sick
-    2 :sick-building
-    3 :healthy-building
-    4 :healthy
-    5 :unknown))
-
-(defn- generate-projects [^StringReader source _]
-  (let [tray-id (.toString source)
-        rnd (Random. (.hashCode tray-id))
-        projects-to-generate (range (+ 3 (.nextInt rnd 97)))]
-    (map (fn [_] (let [prognosis (rand-prognosis rnd)
-                       name (str (rand-str rnd) (s/replace prognosis #"[-:]" " "))]
-                   {:last-build-label  (str (.nextInt rnd 10000))
-                    :last-build-time   (-> (Clock/systemUTC)
-                                           (.instant)
-                                           (.minusSeconds (.nextInt rnd 86401)))
-                    :name              name
-                    :prognosis         prognosis
-                    :unnormalised-name name
-                    :web-url           "https://github.com/build-canaries/nevergreen"
-                    :tray-id           tray-id}))
-         projects-to-generate)))
+            [clojure.java.io :as io]))
 
 (defn preview [request]
-  ; fetch needs to return something closeable
-  (binding [projects/*fetch* (fn [feed] (StringReader. (:tray-id feed)))
-            projects/*parse* generate-projects]
+  (binding [projects/*fetch* (fn [_] (io/input-stream (io/resource "preview.xml")))]
     (projects/get-projects request)))
