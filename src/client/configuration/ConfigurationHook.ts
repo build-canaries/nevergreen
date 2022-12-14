c310a4038f85fd6f1989b66314cdb41ac23fe58d
Christopher Martin <eternalepoch@gmail.com>
2022-12-14 14:45:00 +0000

    use react query to load configuration

--- src/client/configuration/ConfigurationHook.ts
+++ src/client/configuration/ConfigurationHook.ts
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
 import { init, load } from './LocalRepository'
 import { DataSource, toConfiguration } from './Configuration'
 import { configurationImported } from '../settings/backup/BackupActionCreators'
 import { isRight } from 'fp-ts/Either'
-import * as logger from '../common/Logger'
-import join from 'lodash/join'
 import { useAppDispatch } from './Hooks'
+import { useQuery } from '@tanstack/react-query'
+import { info } from '../common/Logger'
 
 interface Result {
   readonly loaded: boolean
@@ -14,42 +13,25 @@ interface Result {
 
 export function useLocalConfiguration(): Result {
   const dispatch = useAppDispatch()
-  const [loaded, setLoaded] = useState(false)
-  const [error, setError] = useState(false)
 
-  useEffect(() => {
-    setLoaded(false)
+  const { isError, isLoading } = useQuery(['load-config'], async () => {
+    info('Loading local configuration')
 
-    const loadData = async () => {
-      try {
-        await init()
-        const untrustedData = await load()
-        const result = toConfiguration(untrustedData, DataSource.systemImport)
+    await init()
+    const untrustedData = await load()
+    const result = toConfiguration(untrustedData, DataSource.systemImport)
 
-        if (isRight(result)) {
-          dispatch(configurationImported(result.right))
-        } else {
-          logger.error(
-            `Unable to initialise Nevergreen because of configuration validation errors, ${join(
-              result.left,
-              ', '
-            )}`
-          )
-          setError(true)
-        }
-      } catch (e) {
-        logger.error(
-          'Unable to initialise Nevergreen because of configuration loading error',
-          e
-        )
-        setError(true)
-      }
-
-      setLoaded(true)
+    if (isRight(result)) {
+      dispatch(configurationImported(result.right))
+      return result.right
+    } else {
+      throw new Error(
+        `Unable to initialise Nevergreen because of configuration validation errors, ${result.left.join(
+          ','
+        )}`
+      )
     }
+  })
 
-    void loadData()
-  }, [dispatch])
-
-  return { loaded, error }
+  return { loaded: !isLoading, error: isError }
 }
