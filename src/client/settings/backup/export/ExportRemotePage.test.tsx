a1d4b4e10854956f479d2205c975f865a2b50eab
Christopher Martin <eternalepoch@gmail.com>
2023-06-02 18:44:21 +0100

    fix(backup): export twice if a new location
    Refs: #347

--- src/client/settings/backup/export/ExportRemotePage.test.tsx
+++ src/client/settings/backup/export/ExportRemotePage.test.tsx
@@ -5,10 +5,12 @@ import * as BackupGateway from '../../../gateways/BackupGateway'
 import { screen, waitFor } from '@testing-library/react'
 import { remoteLocationsRoot } from '../RemoteLocationsReducer'
 import { personalSettingsRoot } from '../../PersonalSettingsReducer'
+import { RemoteLocationOptions } from '../RemoteLocationOptions'
 
 it('should export configuration', async () => {
   const remoteLocation = buildRemoteBackupLocation({
     internalId: 'locationId',
+    externalId: 'some-remote-id',
   })
   const state = {
     [remoteLocationsRoot]: {
@@ -36,6 +38,91 @@ it('should export configuration', async () => {
       screen.getByText('Successfully exported configuration')
     ).toBeInTheDocument()
   })
+  expect(BackupGateway.exportConfiguration).toHaveBeenCalledTimes(1)
+})
+
+it.each([RemoteLocationOptions.gitHub, RemoteLocationOptions.gitLab])(
+  'should export configuration to a new %s location twice to make sure the exported configuration contains the external ID',
+  async (where) => {
+    const remoteLocation = buildRemoteBackupLocation({
+      where,
+      internalId: 'locationId',
+      externalId: '',
+    })
+    const state = {
+      [remoteLocationsRoot]: {
+        locationId: remoteLocation,
+      },
+      [personalSettingsRoot]: {
+        backupRemoteLocations: {
+          locationId: {},
+        },
+      },
+    }
+    jest.spyOn(BackupGateway, 'exportConfiguration').mockResolvedValue({
+      id: 'some-remote-id',
+    })
+
+    const { user } = render(<ExportRemotePage />, {
+      state,
+      outletContext: remoteLocation,
+    })
+
+    await user.click(screen.getByRole('button', { name: 'Export' }))
+
+    await waitFor(() => {
+      expect(
+        screen.getByText('Successfully exported configuration')
+      ).toBeInTheDocument()
+    })
+    expect(BackupGateway.exportConfiguration).toHaveBeenCalledTimes(2)
+    expect(BackupGateway.exportConfiguration).toHaveBeenNthCalledWith(
+      1,
+      remoteLocation,
+      expect.stringContaining('"externalId": ""'),
+      expect.any(AbortSignal)
+    )
+    expect(BackupGateway.exportConfiguration).toHaveBeenNthCalledWith(
+      2,
+      remoteLocation,
+      expect.stringContaining('"externalId": "some-remote-id"')
+    )
+  }
+)
+
+it('should not export configuration to a new custom location twice, as custom locations do not use external IDs', async () => {
+  const remoteLocation = buildRemoteBackupLocation({
+    where: RemoteLocationOptions.custom,
+    internalId: 'locationId',
+    externalId: '',
+  })
+  const state = {
+    [remoteLocationsRoot]: {
+      locationId: remoteLocation,
+    },
+    [personalSettingsRoot]: {
+      backupRemoteLocations: {
+        locationId: {},
+      },
+    },
+  }
+  jest.spyOn(BackupGateway, 'exportConfiguration').mockResolvedValueOnce({
+    id: 'some-remote-id',
+  })
+
+  const { user } = render(<ExportRemotePage />, {
+    state,
+    outletContext: remoteLocation,
+  })
+
+  await user.click(screen.getByRole('button', { name: 'Export' }))
+
+  await waitFor(() => {
+    expect(
+      screen.getByText('Successfully exported configuration')
+    ).toBeInTheDocument()
+  })
+  expect(BackupGateway.exportConfiguration).toHaveBeenCalledTimes(1)
 })
 
 it('should allow cancelling back to settings', async () => {
--- src/client/settings/backup/export/ExportRemotePage.tsx
+++ src/client/settings/backup/export/ExportRemotePage.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from 'react'
+import { useEffect, useState } from 'react'
 import { toExportableConfigurationJson } from '../../../configuration/Configuration'
 import { TextArea } from '../TextArea'
 import { exportConfiguration } from '../../../gateways/BackupGateway'
@@ -10,15 +11,41 @@ import { useRemoteLocationContext } from '../RemoteLocationPage'
 import { ROUTE_BACKUP } from '../../../AppRoutes'
 import { useAppDispatch, useAppSelector } from '../../../configuration/Hooks'
 import { backupExported } from '../RemoteLocationsActions'
+import { isBlank } from '../../../common/Utils'
+import { RemoteLocationOptions } from '../RemoteLocationOptions'
+
+const usesExternalId = [
+  RemoteLocationOptions.gitHub,
+  RemoteLocationOptions.gitLab,
+]
 
 export function ExportRemotePage(): ReactElement {
   const location = useRemoteLocationContext()
   const dispatch = useAppDispatch()
   const configuration = useAppSelector(toExportableConfigurationJson)
+  const [shouldTrigger2ndExport, setShouldTrigger2ndExport] = useState(false)
+
+  /*
+   * We have to trigger the 2nd export in an effect as we need the configuration
+   * to have been updated correctly by the reducers for it to contain the
+   * external id.
+   */
+  useEffect(() => {
+    if (shouldTrigger2ndExport) {
+      void exportConfiguration(location, configuration)
+      setShouldTrigger2ndExport(false)
+    }
+  }, [shouldTrigger2ndExport, location, configuration])
 
   const exportNow = async (signal?: AbortSignal) => {
-    const res = await exportConfiguration(location, configuration, signal)
-    dispatch(backupExported(location.internalId, res.id))
+    const { id } = await exportConfiguration(location, configuration, signal)
+    dispatch(backupExported(location.internalId, id))
+    const aborted = signal?.aborted ?? false
+    setShouldTrigger2ndExport(
+      !aborted &&
+        isBlank(location.externalId) &&
+        usesExternalId.includes(location.where)
+    )
     return { successMessage: 'Successfully exported configuration' }
   }
 
