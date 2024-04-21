76fb58ed39fd0ccb5e8f6d252b0f76e09d46a3e4
Christopher Martin <eternalepoch@gmail.com>
2024-04-21 21:52:40 +0100

    build(deps): updates server deps

--- project.clj
+++ project.clj
@@ -2,15 +2,14 @@
   :description "A build monitor with attitude"
   :url "https://github.com/build-canaries/nevergreen"
   ; use lein deps :tree to check dependency problems and actual versions used
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring "1.10.0" :exclusions [org.eclipse.jetty/jetty-server]]
-                 [org.eclipse.jetty/jetty-server "9.4.53.v20231009"]
-                 [compojure "1.7.0" :exclusions [ring/ring-codec]]
+  :dependencies [[org.clojure/clojure "1.11.2"]
+                 [ring "1.12.1"]
+                 [compojure "1.7.1" :exclusions [ring/ring-codec]]
                  [environ "1.2.0"]
-                 [cheshire "5.12.0"]
+                 [cheshire "5.13.0"]
                  [clj-cctray "2.1.1"]
                  ; commons-* are excluded as ring pulls in newer versions
-                 [clj-http "3.12.3" :exclusions [commons-codec commons-io]]
+                 [clj-http "3.13.0" :exclusions [commons-codec commons-io]]
                  [ring-curl "1.0.1"]
                  [ring/ring-json "0.5.1"]
                  [ring/ring-defaults "0.4.0"]
@@ -19,10 +18,10 @@
                  [base64-clj "0.1.1"]
                  [camel-snake-kebab "0.4.3"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
-                 [ch.qos.logback/logback-classic "1.4.14" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/log4j-over-slf4j "2.0.9"]
-                 [org.slf4j/jul-to-slf4j "2.0.9"]
-                 [org.slf4j/jcl-over-slf4j "2.0.9"]]
+                 [ch.qos.logback/logback-classic "1.5.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/log4j-over-slf4j "2.0.13"]
+                 [org.slf4j/jul-to-slf4j "2.0.13"]
+                 [org.slf4j/jcl-over-slf4j "2.0.13"]]
   :min-lein-version "2.0.0"
   :uberjar-name "nevergreen-standalone.jar"
   :main nevergreen.app
