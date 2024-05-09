1113cf22218f0ccf12a16f40ca863da1d2516eda
Christopher Martin <eternalepoch@gmail.com>
2024-05-10 00:33:51 +0100

    fix: updates add feed help

--- src/client/settings/tracking/AddFeedHelp.tsx
+++ src/client/settings/tracking/AddFeedHelp.tsx
@@ -50,26 +50,32 @@ export function AddFeedHelp({ searchQuery }: HelpProps): ReactElement {
               The entered token will be sent in the <em>Authorization</em>{' '}
               header using the <em>Bearer</em> scheme.
             </dd>
+            <dt>Query parameter</dt>
+            <dd>
+              Select this if access to the feed is protected by a query
+              parameter. The entered key and value will be appended to the URL.
+            </dd>
           </dl>
         </HelpInput>
-        <HelpInput name="Username">
-          The username to send when using basic auth. Only shown if basic auth
-          is selected.
-        </HelpInput>
-        <HelpInput name="Password">
-          The password to send when using basic auth. Only shown if basic auth
-          is selected.
-        </HelpInput>
-        <HelpInput name="Token">
-          The token to send when using access token auth. Only shown if access
-          token authentication is selected.
-        </HelpInput>
+        <p>Shown if &quot;Basic auth&quot; Authentication is selected</p>
+        <HelpInput name="Username">The username to send.</HelpInput>
+        <HelpInput name="Password">The password to send.</HelpInput>
+        <p>Shown if &quot;Access token&quot; Authentication is selected</p>
+        <HelpInput name="Token">The token to send.</HelpInput>
+        <p>Shown if &quot;Query parameter&quot; Authentication is selected</p>
+        <HelpInput name="Query key">The query key to send.</HelpInput>
+        <HelpInput name="Query value">The query value to send.</HelpInput>
         <HelpInput name="Check connection" icon={<Loop />}>
           This will check the currently entered details and display a message
           whether the connection was successful or not.
         </HelpInput>
       </HelpForm>
-      <InfoMessages messages="Passwords and tokens get encrypted by the Nevergreen server and are never stored in plain text in your browser." />
+      <InfoMessages
+        messages={[
+          'Passwords and tokens get encrypted by the Nevergreen server and are never stored in plain text in your browser.',
+          'Make sure to use Query parameter Authentication rather than adding the query parameter directly to the URL. This will ensure the value gets encrypted.',
+        ]}
+      />
       <p>
         Multiple CCTray XML feeds can be tracked,{' '}
         <ExternalLink href="https://www.thoughtworks.com/radar/techniques/a-single-ci-instance-for-all-teams">
